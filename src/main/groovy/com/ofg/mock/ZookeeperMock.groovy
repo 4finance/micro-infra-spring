5c5ca85a8da316ec6db0e1fc1a84568a92e3f07a
szimano <szimano@szimano.org>
2014-06-20 15:46:51 +0200

    zookeeper mock with local server to stop it

--- src/main/groovy/com/ofg/mock/ZookeeperMock.groovy
+++ src/main/groovy/com/ofg/mock/ZookeeperMock.groovy
@@ -1,34 +1,58 @@
 package com.ofg.mock
-
 import groovy.transform.TypeChecked
 import org.apache.curator.test.TestingServer
+import org.jboss.resteasy.test.NettyContainer
+
+import javax.ws.rs.GET
+import javax.ws.rs.Path
 
 @TypeChecked
+@Path("/")
 class ZookeeperMock {
+    static TestingServer testingServer
+
+    @GET
+    @Path("/stop")
+    public void stop() {
+        println "Stopping the zookeeper mock"
+        testingServer.close()
+        NettyContainer.stop()
+    }
+
     public static void main(String[] args) {
         int port = 2181
+        int mockPort = 18081
 
-        if (args.size() > 1) {
+        if (args.size() > 2) {
             printUsage()
             return
         }
-
-        if (args.size() == 1) {
+        if (args.size() >= 1) {
             if (!args[0].isInteger()) {
                 printUsage()
                 return
             }
-
             port = args[0].toInteger()
         }
-
-        TestingServer testingServer = new TestingServer(port)
+        if (args.size() == 2) {
+            if (!args[1].isInteger()) {
+                printUsage()
+                return
+            }
+            mockPort = args[1].toInteger()
+        }
+        System.setProperty("org.jboss.resteasy.port", "$mockPort")
+        NettyContainer.start().getRegistry().addPerRequestResource(ZookeeperMock);
+        testingServer = new TestingServer(port)
+        println "ZooKeeper mock started..."
     }
 
-    static void printUsage() {
+    private static void printUsage() {
         println "USAGE:\n"
-        println "\tjava -jar micro-deps-VERSION-fatJar.jar [portNumber]\n"
+        println "\tjava -jar micro-deps-VERSION-fatJar.jar [portNumber, [mockPortNumber]] \n"
         println "\t\tWHERE\n"
         println "\t\tportNumber: optional port number on which zookeeper mock will be started. Default is 2181\n"
+        println "\t\tmockPortNumber: optional port number on which zookeeper rest server will be started. " +
+                "It will expose one method on /stop to stop the server. Default is 18081\n"
     }
 }

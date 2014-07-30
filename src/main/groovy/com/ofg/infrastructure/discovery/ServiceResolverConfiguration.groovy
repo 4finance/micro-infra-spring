04b55ed2b1f00613388c751a8103b3d9f33b949c
Marcin Grzejszczak <marcin@grzejszczak.pl>
2014-07-30 12:46:34 +0200

    Made JSON configuration abstract (allowing to pass microservice json custom paths)

--- src/main/groovy/com/ofg/infrastructure/discovery/ServiceResolverConfiguration.groovy
+++ src/main/groovy/com/ofg/infrastructure/discovery/ServiceResolverConfiguration.groovy
@@ -1,5 +1,4 @@
 package com.ofg.infrastructure.discovery
-
 import com.ofg.infrastructure.discovery.watcher.DependencyWatcher
 import com.ofg.infrastructure.discovery.watcher.presence.DependencyPresenceOnStartupVerifier
 import com.ofg.infrastructure.discovery.watcher.presence.FailOnMissingDependencyOnStartupVerifier
@@ -16,7 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.core.io.ClassPathResource
+import org.springframework.core.io.Resource
 
 @TypeChecked
 @Configuration
@@ -32,7 +31,7 @@ class ServiceResolverConfiguration {
     
     @PackageScope
     @Bean
-    ServiceConfigurationResolver serviceConfigurationResolver(@Value('${microservice.config.file:microservice.json}') ClassPathResource microserviceConfig) {
+    ServiceConfigurationResolver serviceConfigurationResolver(@Value('${microservice.config.file:classpath:microservice.json}') Resource microserviceConfig) {
         return new ServiceConfigurationResolver(microserviceConfig.inputStream.text)
     }
     

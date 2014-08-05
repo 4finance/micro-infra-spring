1780f2d7e1240aa7e954deaf200d0031b1353298
Kamil Szymanski <kamil.szymanski.dev@gmail.com>
2014-08-05 09:19:26 +0200

    grab stubs only once, skip grabbing unused dependencies

--- src/main/groovy/com/ofg/mock/StubLoader.groovy
+++ src/main/groovy/com/ofg/mock/StubLoader.groovy
@@ -3,6 +3,7 @@ package com.ofg.mock
 import com.ofg.infrastructure.discovery.ServiceConfigurationResolver
 import groovy.grape.Grape
 import groovy.transform.ToString
+import org.codehaus.groovy.tools.RootLoader
 
 class StubLoader {
 
@@ -12,14 +13,13 @@ class StubLoader {
     private static final int MAX_PORT = 50000
 
     void loadStubs(ServiceConfigurationResolver resolver, String repository, int zookeeperPort) {
-        List excludes = [[group: 'org.codehaus.groovy', module: 'groovy-all']]
         Grape.addResolver(name: 'dependency-repository', root: repository)
         List<Module> parsedDeps = parseDeps(resolver.dependencies)
         parsedDeps.each { Module module ->
             println "Loading stub $module"
-            Map depToGrab = [group: module.group, module: module.module, version: LATEST_MODULE, classifier: 'shadow']
-            String mainClassFullyQualifiedName = findMainClassFromManifest(depToGrab, excludes)
-            GroovyClassLoader runtimeCS = loadStubJar(depToGrab, excludes)
+            Map depToGrab = [group: module.group, module: module.module, version: LATEST_MODULE, classifier: 'shadow', transitive: false]
+            GroovyClassLoader runtimeCS = loadStubJar(depToGrab)
+            String mainClassFullyQualifiedName = findMainClass(depToGrab, runtimeCS)
             runtimeCS.loadClass(mainClassFullyQualifiedName).main(resolver.basePath, module.unparsedDependency, (nextAvailablePort()).toString(), zookeeperPort.toString())
         }
     }
@@ -53,19 +53,23 @@ class StubLoader {
         }
     }
 
-    private GroovyClassLoader loadStubJar(LinkedHashMap<String, String> depToGrab, List excludes) {
-        GroovyClassLoader runtimeCS = new GroovyClassLoader()
-        Grape.grab([classLoader: runtimeCS, excludes: excludes], depToGrab)
+    private ClassLoader loadStubJar(Map<String, String> depToGrab) {
+        ClassLoader runtimeCS = new GroovyClassLoader()
+        Grape.grab([classLoader: runtimeCS], depToGrab)
         return runtimeCS
     }
 
-    private String findMainClassFromManifest(LinkedHashMap<String, String> depToGrab, List excludes) {
-        GroovyClassLoader classLoader = new GroovyClassLoader(null)
-        Grape.grab([classLoader: classLoader, excludes: excludes], depToGrab)
-        Properties loadedManifest = new Properties()
-        loadedManifest.load(classLoader.getResourceAsStream(MANIFEST_PATH))
-        String mainClassFullyQualifiedName = loadedManifest.getProperty('Main-Class')
-        return mainClassFullyQualifiedName
+    private String findMainClass(Map<String, String> depToGrab, ClassLoader classLoader) {
+        Properties manifest = loadStubManifest(depToGrab, classLoader)
+        return manifest.getProperty('Main-Class')
+    }
+
+    private Properties loadStubManifest(Map<String, String> depToGrab, ClassLoader classLoader) {
+        URL[] classpath = Grape.resolve([classLoader: classLoader], depToGrab).collect { it.toURL() }
+        ClassLoader rootLoader = new RootLoader(classpath, null)
+        Properties manifest = new Properties()
+        manifest.load(rootLoader.getResourceAsStream(MANIFEST_PATH))
+        return manifest
     }
 
     @ToString(includePackage = false, includeNames = true)

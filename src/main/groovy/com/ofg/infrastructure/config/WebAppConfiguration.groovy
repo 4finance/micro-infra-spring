9277a4f172ef408a2a971b8fb0aa54733b328924
Marcin Grzejszczak <marcin@grzejszczak.pl>
2014-08-25 14:44:50 +0200

    Added Swagger resources, modified Swagger Spring configuration and added it to WebAppConfiguration

--- src/main/groovy/com/ofg/infrastructure/config/WebAppConfiguration.groovy
+++ src/main/groovy/com/ofg/infrastructure/config/WebAppConfiguration.groovy
@@ -3,6 +3,7 @@ package com.ofg.infrastructure.config
 import com.ofg.infrastructure.discovery.ServiceDiscoveryConfiguration
 import com.ofg.infrastructure.healthcheck.HealthCheckConfiguration
 import com.ofg.infrastructure.metrics.registry.MetricsRegistryConfiguration
+import com.ofg.infrastructure.web.config.SwaggerConfiguration
 import com.ofg.infrastructure.web.config.WebInfrastructureConfiguration
 import groovy.transform.TypeChecked
 import org.springframework.context.annotation.Configuration
@@ -10,6 +11,6 @@ import org.springframework.context.annotation.Import
 
 @Configuration
 @TypeChecked
-@Import([WebInfrastructureConfiguration, ServiceDiscoveryConfiguration, MetricsRegistryConfiguration, HealthCheckConfiguration])
+@Import([WebInfrastructureConfiguration, ServiceDiscoveryConfiguration, MetricsRegistryConfiguration, HealthCheckConfiguration, SwaggerConfiguration])
 class WebAppConfiguration {
 }

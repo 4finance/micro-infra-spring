8443c8747bc2a97b5aa39bff2042abd403a344ba
Marcin Grzejszczak <marcin@grzejszczak.pl>
2014-08-28 21:35:00 +0200

    Added some javadocs on WebAppConfiguration

--- src/main/groovy/com/ofg/infrastructure/config/WebAppConfiguration.groovy
+++ src/main/groovy/com/ofg/infrastructure/config/WebAppConfiguration.groovy
@@ -9,6 +9,24 @@ import groovy.transform.TypeChecked
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
 
+/**
+ * Imports:
+ * <ul>
+ *  <li>WebInfrastructureConfiguration {@see WebInfrastructureConfiguration}
+ *      <ul>
+ *          <li>RestTemplateConfiguration - default RestTemplate with custom error handling</li>
+ *          <li>ServiceRestClientConfiguration - RestTemplate abstraction with ServiceDiscovery</li>
+ *          <li>ControllerExceptionConfiguration - default Exception handling</li>
+ *          <li>FilterConfiguration - filter for logging request body</li>
+ *          <li>ViewConfiguration - converts unmapped Views to JSON requests</li>
+ *      </ul>
+ *  </li>
+ *  <li> ServiceDiscoveryConfiguration {@see ServiceDiscoveryConfiguration} - contains all beans related to service discovery</li>
+ *  <li> MetricsRegistryConfiguration {@see MetricsRegistryConfiguration} - contains bean with registry of metrics instances
+ *  <li> HealthCheckConfiguration {@see HealthCheckConfiguration} - contains beans related to Health check verification
+ *  <li> SwaggerConfiguration {@see SwaggerConfiguration} - contains beans related to Swagger API documentation
+ * </ul>
+ */
 @Configuration
 @TypeChecked
 @Import([WebInfrastructureConfiguration, ServiceDiscoveryConfiguration, MetricsRegistryConfiguration, HealthCheckConfiguration, SwaggerConfiguration])

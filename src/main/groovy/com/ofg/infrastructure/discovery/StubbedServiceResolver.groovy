6886b89e68fe63a5c8dc1f2090f5bf0072fce15e
Adam Chudzik <Adam.Chudzik@4finance.com>
2014-09-03 18:24:04 +0200

    StubbedServiceResolver moved from micro-deps-spring-test-config project

---
+++ src/main/groovy/com/ofg/infrastructure/discovery/StubbedServiceResolver.groovy
@@ -0,0 +1,43 @@
+package com.ofg.infrastructure.discovery
+
+import com.google.common.base.Optional as GuavaOptional
+import groovy.transform.CompileStatic
+
+@CompileStatic
+class StubbedServiceResolver implements ServiceResolver {
+
+    Map<String, String> stubbedDeps = [:]
+    private final Integer wiremockPort
+    private final String wiremockUrl
+
+    StubbedServiceResolver(Integer wiremockPort,  String wiremockUrl) {
+        this.wiremockPort = wiremockPort
+        this.wiremockUrl = wiremockUrl
+    }
+
+    void stubDependency(String dependency, String address) {
+        stubbedDeps << [(dependency) : address]
+    }
+
+    void resetDependencies() {
+        stubbedDeps.clear()
+    }
+
+    void stubDependenciesFrom(ServiceConfigurationResolver serviceConfigurationResolver) {
+        serviceConfigurationResolver.dependencies.each {
+            stubDependency(it.key, "http://$wiremockUrl:$wiremockPort/${it.key}")
+        }
+    }
+
+    @Override
+    GuavaOptional<String> getUrl(String dependency) {
+        return GuavaOptional.fromNullable(stubbedDeps[dependency])
+    }
+
+    @Override
+    void start() { }
+
+    @Override
+    void close() { }
+
+}

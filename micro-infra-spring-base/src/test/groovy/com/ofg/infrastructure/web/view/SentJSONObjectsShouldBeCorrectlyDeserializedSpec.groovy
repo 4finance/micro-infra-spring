64e6636e0291a579011ef34d79f26c67ea77a8cd
Maciej Ziarko <Maciej.Ziarko@4finance.com>
2014-09-25 18:04:15 +0200

    Additional test to [#33] & [#34]

---
+++ micro-infra-spring-base/src/test/groovy/com/ofg/infrastructure/web/view/SentJSONObjectsShouldBeCorrectlyDeserializedSpec.groovy
@@ -0,0 +1,35 @@
+package com.ofg.infrastructure.web.view
+
+import com.ofg.infrastructure.base.BaseConfiguration
+import com.ofg.infrastructure.base.ConfigurationWithoutServiceDiscovery
+import com.ofg.infrastructure.base.MvcIntegrationSpec
+import org.springframework.boot.test.SpringApplicationContextLoader
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpStatus
+import org.springframework.test.context.ContextConfiguration
+
+import static org.springframework.http.MediaType.APPLICATION_JSON
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+@ContextConfiguration(classes = [Config, BaseConfiguration, ConfigurationWithoutServiceDiscovery],
+        loader = SpringApplicationContextLoader)
+class SentJSONObjectsShouldBeCorrectlyDeserializedSpec extends MvcIntegrationSpec {
+
+    def "should correctly deserialize request to SampleBean instance when valid JSON document is sent to controller"() {
+        expect:
+            mockMvc.perform(post("/test")
+                .contentType(APPLICATION_JSON)
+                .content('{"sampleField":"sampleValue"}'))
+                .andExpect(status().is(HttpStatus.CREATED.value()))
+    }
+
+    @Configuration
+    static class Config {
+        @Bean
+        TestController testController() {
+            return new TestController()
+        }
+    }
+}
--- micro-infra-spring-base/src/test/groovy/com/ofg/infrastructure/web/view/TestController.groovy
+++ micro-infra-spring-base/src/test/groovy/com/ofg/infrastructure/web/view/TestController.groovy
@@ -1,28 +1,36 @@
 package com.ofg.infrastructure.web.view
-
 import groovy.transform.PackageScope
+import groovy.transform.ToString
+import groovy.util.logging.Slf4j
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 
 @PackageScope
 @Controller
-/**
- * Test controller that returns {@link SampleBean } which is then serialized to JSON.
- * In development or test environment JSON is supposed to be pretty printed.
- * In production environment JSON should not be pretty printed.
- */
+@Slf4j
 class TestController {
 
     @RequestMapping(value = "/test", produces = "application/json", method = RequestMethod.GET)
-    ResponseEntity<SampleBean> test() {
+    ResponseEntity<SampleBean> testGet() {
         return new ResponseEntity<SampleBean>(new SampleBean(sampleField: "sampleValue"), HttpStatus.OK)
+    }
+
+    @RequestMapping(value = "/test", consumes = "application/json", produces = "application/json", method = RequestMethod.POST)
+    ResponseEntity<SampleBean> testPost(@RequestBody SampleBean sampleBean) {
+        if (sampleBean.sampleField == "sampleValue") {
+            return new ResponseEntity<SampleBean>(HttpStatus.CREATED)
+        } else {
+            return new ResponseEntity<SampleBean>(HttpStatus.BAD_REQUEST)
+        }
     }
 }
 
 @PackageScope
+@ToString
 class SampleBean {
     String sampleField
 }

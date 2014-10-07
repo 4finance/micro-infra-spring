0eae39991b94b8bb8dc014944b60878040122286
mchmielarz <michal.chmielarz@gmail.com>
2014-10-07 12:47:44 +0200

    Using initMethod to initialize bean class.

--- micro-infra-camel/src/main/groovy/com/ofg/infrastructure/camel/CamelRouteConfiguration.groovy
+++ micro-infra-camel/src/main/groovy/com/ofg/infrastructure/camel/CamelRouteConfiguration.groovy
@@ -1,6 +1,7 @@
 package com.ofg.infrastructure.camel
 
 import groovy.transform.CompileStatic
+import groovy.util.logging.Slf4j
 import org.apache.camel.CamelContext
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -9,7 +10,7 @@ import org.springframework.context.annotation.Configuration
 @CompileStatic
 class CamelRouteConfiguration {
 
-    @Bean
+    @Bean(initMethod = 'addCorrelationIdInterception')
     CamelRouteModifier camelRouteModifier(CamelContext camelContext) {
         return new CamelRouteModifier(camelContext)
     }

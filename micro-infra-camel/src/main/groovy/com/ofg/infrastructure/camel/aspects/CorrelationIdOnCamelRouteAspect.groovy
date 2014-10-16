a6b74966f5442242bab381e219bc5ec0b44010ef
mchmielarz <michal.chmielarz@gmail.com>
2014-10-16 13:41:30 +0200

    Registering correlationId interceptor on Camel routes using aspectj to prevent errors with setting interceptor after all routes are initialized.

---
+++ micro-infra-camel/src/main/groovy/com/ofg/infrastructure/camel/aspects/CorrelationIdOnCamelRouteAspect.groovy
@@ -0,0 +1,36 @@
+package com.ofg.infrastructure.camel.aspects;
+
+import com.ofg.infrastructure.camel.CorrelationIdInterceptor
+import groovy.transform.CompileStatic
+import groovy.util.logging.Slf4j;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.springframework.stereotype.Component;
+
+import static org.slf4j.LoggerFactory.*;
+
+/**
+ * Aspect that adds {@link CorrelationIdInterceptor} to {@link RouteBuilder} for all incoming messages
+ * and every message send to any endpoint.
+ */
+@Slf4j
+@Aspect
+@CompileStatic
+class CorrelationIdOnCamelRouteAspect {
+
+    private static final String ANY = '*'
+    private final Processor correlationIdInterceptor = new CorrelationIdInterceptor()
+
+    @Around(value = "execution(* org.apache.camel.builder.RouteBuilder.addRoutesToCamelContext(..))")
+    Object aroundAddRoutesNoArgs(ProceedingJoinPoint joinPoint) throws Throwable {
+        RouteBuilder targetRouteBuilder = (RouteBuilder)joinPoint.getTarget()
+        log.debug("Setting correlationId interception on ${targetRouteBuilder.getClass().getName()}")
+        targetRouteBuilder.interceptFrom().process(correlationIdInterceptor)
+        targetRouteBuilder.interceptSendToEndpoint(ANY).process(correlationIdInterceptor)
+        return joinPoint.proceed()
+    }
+}
---
+++ micro-infra-spring-base/src/main/groovy/com/ofg/infrastructure/discovery/CollaboratorsConnectivityController.groovy
@@ -0,0 +1,16 @@
+package com.ofg.infrastructure.discovery
+
+import groovy.transform.CompileStatic
+import groovy.util.logging.Slf4j
+import org.springframework.web.bind.annotation.RestController
+
+@Slf4j
+@CompileStatic
+@RestController
+class CollaboratorsConnectivityController {
+
+    String getCollaboratorsHealthInfo() {
+
+    }
+
+}

d0c23c2c14abc1af3277436302590591d829ca81
mchmielarz <michal.chmielarz@gmail.com>
2014-10-24 11:12:33 +0200

    Logging recent changes on status connection with dependent service.

--- src/main/groovy/com/ofg/infrastructure/discovery/watcher/DependencyStateChangeListenerRegistry.groovy
+++ src/main/groovy/com/ofg/infrastructure/discovery/watcher/DependencyStateChangeListenerRegistry.groovy
@@ -2,11 +2,15 @@ package com.ofg.infrastructure.discovery.watcher
 
 import groovy.transform.CompileStatic
 import groovy.transform.PackageScope
+import groovy.util.logging.Slf4j
 import org.apache.curator.framework.CuratorFramework
 import org.apache.curator.framework.state.ConnectionState
 import org.apache.curator.x.discovery.ServiceCache
 import org.apache.curator.x.discovery.details.ServiceCacheListener
 
+import static com.ofg.infrastructure.discovery.watcher.DependencyState.*
+
+@Slf4j
 @PackageScope
 @CompileStatic
 class DependencyStateChangeListenerRegistry implements ServiceCacheListener {
@@ -23,9 +27,15 @@ class DependencyStateChangeListenerRegistry implements ServiceCacheListener {
 
     @Override
     void cacheChanged() {
+        DependencyState state = serviceCache.instances.empty ? DISCONNECTED : CONNECTED
+        logCurrentState(state)
         listeners.each {
-            it.stateChanged(dependencyName, serviceCache.instances.empty ? DependencyState.DISCONNECTED : DependencyState.CONNECTED)
+            it.stateChanged(dependencyName, state)
         }
+    }
+
+    private void logCurrentState(DependencyState dependencyState) {
+        log.info("State of '${dependencyName}' service changed to ${dependencyState}.")
     }
 
     @Override

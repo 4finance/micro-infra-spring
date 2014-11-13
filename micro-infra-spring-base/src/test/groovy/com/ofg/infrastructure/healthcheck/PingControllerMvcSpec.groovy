d3eb9521c539c21c39acc15b21465003a99bc633
mchmielarz <michal.chmielarz@gmail.com>
2014-11-13 22:04:07 +0100

    Duplication of ContextConfiguration removed.

--- micro-infra-spring-base/src/test/groovy/com/ofg/infrastructure/healthcheck/PingControllerMvcSpec.groovy
+++ micro-infra-spring-base/src/test/groovy/com/ofg/infrastructure/healthcheck/PingControllerMvcSpec.groovy
@@ -15,7 +15,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
-@ContextConfiguration(classes = [BaseConfiguration, ServiceDiscoveryStubbingApplicationConfiguration, HealthCheckConfiguration], loader = SpringApplicationContextLoader)
 @ContextConfiguration(classes = TestConfig, loader = SpringApplicationContextLoader)
 class PingControllerMvcSpec extends MvcCorrelationIdSettingIntegrationSpec {
     

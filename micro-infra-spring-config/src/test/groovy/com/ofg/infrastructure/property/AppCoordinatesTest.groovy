8db4a57ead493e1a788e01c49164ddb8dca8329d
Tomasz Nurkiewicz <tomasz.nurkiewicz@4finance.com>
2014-12-01 10:49:55 +0100

    [#83] One extra test for real-life path

--- micro-infra-spring-config/src/test/groovy/com/ofg/infrastructure/property/AppCoordinatesTest.groovy
+++ micro-infra-spring-config/src/test/groovy/com/ofg/infrastructure/property/AppCoordinatesTest.groovy
@@ -15,11 +15,30 @@ class AppCoordinatesTest extends Specification {
             coords.getConfigFiles(new File('/home')) == paths.collect { new File(new File('/home'), it) }
 
         where:
-            env    | country | name                   || paths
-            'prod' | 'pl'    | 'foo'                  || ['/prod/foo.properties', '/prod/foo.yaml', '/prod/pl/foo-pl.properties', '/prod/pl/foo-pl.yaml']
-            'prod' | 'pl'    | '/a/b/c'               || ['/prod/a/b/c.properties', '/prod/a/b/c.yaml', '/prod/a/b/pl/c-pl.properties', '/prod/a/b/pl/c-pl.yaml']
-            'dev'  | 'es'    | '/com/ofg/fraud-es'    || ['/dev/com/ofg/fraud.properties', '/dev/com/ofg/fraud.yaml', '/dev/com/ofg/es/fraud-es.properties', '/dev/com/ofg/es/fraud-es.yaml']
-            'dev'  | 'pl'    | '/com/ofg/pl/fraud-pl' || ['/dev/com/ofg/fraud.properties', '/dev/com/ofg/fraud.yaml', '/dev/com/ofg/pl/fraud-pl.properties', '/dev/com/ofg/pl/fraud-pl.yaml']
-            'dev'  | 'pl'    | '/com/ofg/pl/fraud'    || ['/dev/com/ofg/fraud.properties', '/dev/com/ofg/fraud.yaml', '/dev/com/ofg/pl/fraud-pl.properties', '/dev/com/ofg/pl/fraud-pl.yaml']
+            env        | country | name                                   || paths
+            'prod'     | 'pl'    | 'foo'                                  || ['/prod/foo.properties',
+                                                                              '/prod/foo.yaml',
+                                                                              '/prod/pl/foo-pl.properties',
+                                                                              '/prod/pl/foo-pl.yaml']
+            'prod'     | 'pl'    | '/a/b/c'                               || ['/prod/a/b/c.properties',
+                                                                              '/prod/a/b/c.yaml',
+                                                                              '/prod/a/b/pl/c-pl.properties',
+                                                                              '/prod/a/b/pl/c-pl.yaml']
+            'dev'      | 'es'    | '/com/ofg/fraud-es'                    || ['/dev/com/ofg/fraud.properties',
+                                                                              '/dev/com/ofg/fraud.yaml',
+                                                                              '/dev/com/ofg/es/fraud-es.properties',
+                                                                              '/dev/com/ofg/es/fraud-es.yaml']
+            'dev'      | 'pl'    | '/com/ofg/pl/fraud-pl'                 || ['/dev/com/ofg/fraud.properties',
+                                                                              '/dev/com/ofg/fraud.yaml',
+                                                                              '/dev/com/ofg/pl/fraud-pl.properties',
+                                                                              '/dev/com/ofg/pl/fraud-pl.yaml']
+            'dev'      | 'pl'    | '/com/ofg/pl/fraud'                    || ['/dev/com/ofg/fraud.properties',
+                                                                              '/dev/com/ofg/fraud.yaml',
+                                                                              '/dev/com/ofg/pl/fraud-pl.properties',
+                                                                              '/dev/com/ofg/pl/fraud-pl.yaml']
+            'stage-01' | 'ro'    | 'com/ofg/loans/ro/backoffice-vivus-ro' || ['/stage-01/com/ofg/loans/backoffice-vivus.properties',
+                                                                              '/stage-01/com/ofg/loans/backoffice-vivus.yaml',
+                                                                              '/stage-01/com/ofg/loans/ro/backoffice-vivus-ro.properties',
+                                                                              '/stage-01/com/ofg/loans/ro/backoffice-vivus-ro.yaml']
     }
 }

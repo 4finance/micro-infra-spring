f260183bf404371d78cf745efffc03fd7a91de9e
Kamil Szymanski <kamil.szymanski.dev@gmail.com>
2014-12-12 13:12:48 +0100

    MetricPathProvider refactoring

--- micro-infra-spring-base/src/main/groovy/com/ofg/infrastructure/metrics/registry/MetricPathProvider.groovy
+++ micro-infra-spring-base/src/main/groovy/com/ofg/infrastructure/metrics/registry/MetricPathProvider.groovy
@@ -2,7 +2,6 @@ package com.ofg.infrastructure.metrics.registry
 
 import com.google.common.base.Joiner
 import groovy.transform.CompileStatic
-import groovy.transform.Memoized
 
 /**
  * This class is responsible for preparing your metric full path. It also verifies whether your metric name is already  
@@ -22,32 +21,17 @@ import groovy.transform.Memoized
  */
 @CompileStatic
 class MetricPathProvider {
-
-    private final String environment
-    private final String rootName
-    private final String country
-    private final String appName
+    private final String metricPathPrefix
 
     MetricPathProvider(String rootName, String environment, String country, String appName) {
-        this.rootName = rootName
-        this.environment = environment
-        this.country = country
-        this.appName = appName
-    }
-
-    @Memoized
-    private String getRootMetricPath() {
-        return Joiner.on('.').join(rootName, environment, country, appName)
+        metricPathPrefix = Joiner.on('.').join(rootName, environment, country, appName)
     }
 
     String getMetricPath(String metricName) {
-        if (isPathPrepended(metricName)) {
-            return metricName
-        }
-        return Joiner.on('.').join(rootMetricPath, metricName)
+        return isPathPrepended(metricName) ? metricName : Joiner.on('.').join(metricPathPrefix, metricName)
     }
 
     boolean isPathPrepended(String metricName) {
-        return metricName.startsWith(rootMetricPath)
+        return metricName.startsWith(metricPathPrefix)
     }
 }

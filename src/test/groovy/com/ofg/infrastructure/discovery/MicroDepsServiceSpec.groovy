6a8ce413e60d017c307d8bf0bb679153e9d6e0e8
mchmielarz <michal.chmielarz@gmail.com>
2014-12-16 11:36:15 +0100

    [#29] Indentations for better readability.

--- src/test/groovy/com/ofg/infrastructure/discovery/MicroDepsServiceSpec.groovy
+++ src/test/groovy/com/ofg/infrastructure/discovery/MicroDepsServiceSpec.groovy
@@ -77,51 +77,51 @@ class MicroDepsServiceSpec extends Specification {
 
     def "should setup service discovery properly"() {
         given:
-        def microAService =
-                new MicroDepsService(server.connectString, "rest", "microAUrl", 8877, MICRO_A)
+            def microAService =
+                    new MicroDepsService(server.connectString, "rest", "microAUrl", 8877, MICRO_A)
 
-        def microBService =
-                new MicroDepsService(server.connectString, "rest", "microBUrl", 8888,  MICRO_B)
+            def microBService =
+                    new MicroDepsService(server.connectString, "rest", "microBUrl", 8888,  MICRO_B)
 
-        String dep = null
-        DependencyState state = null
+            String dep = null
+            DependencyState state = null
 
-        DependencyWatcherListener listener = new DependencyWatcherListener() {
-            @Override
-            void stateChanged(String dependencyName, DependencyState newState) {
-                dep = dependencyName
-                state = newState
+            DependencyWatcherListener listener = new DependencyWatcherListener() {
+                @Override
+                void stateChanged(String dependencyName, DependencyState newState) {
+                    dep = dependencyName
+                    state = newState
+                }
             }
-        }
 
         when:
-        microAService.start()
+            microAService.start()
 
-        microBService.registerDependencyStateChangeListener(listener)
-        microBService.start()
+            microBService.registerDependencyStateChangeListener(listener)
+            microBService.start()
 
-        microAService.stop()
+            microAService.stop()
 
-        await().atMost(5, TimeUnit.SECONDS).until {
-            dep == "microA" && state == DependencyState.DISCONNECTED
-        }
+            await().atMost(5, TimeUnit.SECONDS).until {
+                dep == "microA" && state == DependencyState.DISCONNECTED
+            }
 
         then:
-        noExceptionThrown()
+            noExceptionThrown()
 
         when:
-        microAService = new MicroDepsService(server.connectString, "rest", "microAUrl", 8877, MICRO_A)
-        microAService.start()
+            microAService = new MicroDepsService(server.connectString, "rest", "microAUrl", 8877, MICRO_A)
+            microAService.start()
 
-        await().atMost(5, TimeUnit.SECONDS).until {
-            dep == "microA" && state == DependencyState.CONNECTED
-        }
+            await().atMost(5, TimeUnit.SECONDS).until {
+                dep == "microA" && state == DependencyState.CONNECTED
+            }
 
         then:
-        noExceptionThrown()
+            noExceptionThrown()
 
         cleanup:
-        microBService?.stop()
-        microAService?.stop()
+            microBService?.stop()
+            microAService?.stop()
     }
 }

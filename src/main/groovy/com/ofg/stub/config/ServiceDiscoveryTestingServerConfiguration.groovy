c0ec5c6721c45314e0825db82c0791241375715a
mchmielarz <michal.chmielarz@gmail.com>
2014-12-18 11:12:01 +0100

    Fix #14 - Set random port of TestingServer when not provided

--- src/main/groovy/com/ofg/stub/config/ServiceDiscoveryTestingServerConfiguration.groovy
+++ src/main/groovy/com/ofg/stub/config/ServiceDiscoveryTestingServerConfiguration.groovy
@@ -1,23 +1,29 @@
 package com.ofg.stub.config
 
+import com.google.common.base.Strings
 import com.google.common.net.HostAndPort
 import groovy.transform.CompileStatic
 import org.apache.curator.test.TestingServer
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.util.SocketUtils
 
 @Configuration
 @CompileStatic
 class ServiceDiscoveryTestingServerConfiguration {
 
     /**
-     * Test instance of Zookeeper
+     * Provides test instance of Zookeeper. If {@code serviceResolverUrl} is {@code null} or an empty string a randomly selected, available port is used.
      *
-     * @param serviceResolverUrl - host with port where your application where search for Zookeeper instance
+     * @param serviceResolverUrl - host with port where your application will search for Zookeeper instance
+     * 
+     * @throws IllegalArgumentException when port in provided URL is not parseable, i.e. it is not numeric value
      */
     @Bean(destroyMethod = 'close')
-    TestingServer testingServer(@Value('${service.resolver.url:localhost:2181}') String serviceResolverUrl) {
-        return new TestingServer(HostAndPort.fromString(serviceResolverUrl).port)
+    TestingServer testingServer(@Value('${service.resolver.url:}') String serviceResolverUrl) {
+        String serviceUrl = Strings.nullToEmpty(serviceResolverUrl)
+        int defaultPort = SocketUtils.findAvailableTcpPort()
+        return new TestingServer(HostAndPort.fromString(serviceUrl).getPortOrDefault(defaultPort))
     }
 }

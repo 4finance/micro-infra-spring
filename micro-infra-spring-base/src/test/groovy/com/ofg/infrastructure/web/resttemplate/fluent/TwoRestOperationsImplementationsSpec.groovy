08ea7e72e338edc987b82844cd02d127cc43bb79
Tomasz Nurkiewicz <tomasz.nurkiewicz@4finance.com>
2014-12-21 16:01:48 +0100

    Manually created stub (Java proxy) - I don't want to change this every time ServieResolve changes and Spock can't mock here (only in tests)

--- micro-infra-spring-base/src/test/groovy/com/ofg/infrastructure/web/resttemplate/fluent/TwoRestOperationsImplementationsSpec.groovy
+++ micro-infra-spring-base/src/test/groovy/com/ofg/infrastructure/web/resttemplate/fluent/TwoRestOperationsImplementationsSpec.groovy
@@ -4,6 +4,7 @@ import com.ofg.infrastructure.base.BaseConfiguration
 import com.ofg.infrastructure.base.MvcCorrelationIdSettingIntegrationSpec
 import com.ofg.infrastructure.discovery.ServiceConfigurationResolver
 import com.ofg.infrastructure.discovery.ServiceResolver
+import com.ofg.infrastructure.discovery.ServiceUnavailableException
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.SpringApplicationContextLoader

cb1d1dfa243550c77361ebfcd7eee351c46212c7
Tomasz Nurkiewicz <tomasz.nurkiewicz@4finance.com>
2014-12-21 13:48:35 +0100

    Unwrapping ExecutionException when executed synchronously. Fixes #139

--- micro-infra-spring-base/src/test/groovy/com/ofg/infrastructure/web/resttemplate/fluent/common/response/executor/RestExecutorTest.groovy
+++ micro-infra-spring-base/src/test/groovy/com/ofg/infrastructure/web/resttemplate/fluent/common/response/executor/RestExecutorTest.groovy
@@ -3,6 +3,7 @@ package com.ofg.infrastructure.web.resttemplate.fluent.common.response.executor
 import com.nurkiewicz.asyncretry.SyncRetryExecutor
 import com.ofg.infrastructure.web.resttemplate.custom.RestTemplate
 import org.springframework.http.HttpMethod
+import org.springframework.web.client.ResourceAccessException
 import spock.lang.Specification
 
 class RestExecutorTest extends Specification {
@@ -17,6 +18,18 @@ class RestExecutorTest extends Specification {
         then:
             IllegalStateException e = thrown(IllegalStateException)
             e.message.contains("retryUsing")
+    }
+
+    def 'should propagate original RestTemplate exception'() {
+        given:
+            RestExecutor executor = new RestExecutor(
+                    new RestTemplate(), SyncRetryExecutor.INSTANCE)
+        when:
+            executor.exchange(HttpMethod.GET, [host: 'http://localhost:7777', url: '/api'.toURI()], Object)
+
+        then:
+            ResourceAccessException e = thrown(ResourceAccessException)
+            e.message.contains('localhost:7777')
     }
 
 }

f9c1d7325a1191c794c41e18f98240f28b2d1029
Tomasz Nurkiewicz <tomasz.nurkiewicz@4finance.com>
2014-12-23 12:10:38 +0100

    ResponseRethrowingErrorHandler fails in getBody() when handling exceptions Fixes #144

---
+++ micro-infra-spring-base/src/test/groovy/com/ofg/infrastructure/web/resttemplate/custom/RestTemplateSpec.groovy
@@ -0,0 +1,78 @@
+package com.ofg.infrastructure.web.resttemplate.custom
+import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.client.WireMock
+import spock.lang.AutoCleanup
+import spock.lang.Shared
+import spock.lang.Specification
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse
+import static com.github.tomakehurst.wiremock.client.WireMock.get
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo
+import static org.springframework.http.HttpStatus.NOT_FOUND
+
+class RestTemplateSpec extends Specification {
+
+    @Shared
+    @AutoCleanup("stop")
+    private static WireMockServer wireMockServer = new WireMockServer(0)
+
+    @Shared
+    private static int port
+
+    def setupSpec() {
+        wireMockServer.start()
+        port = wireMockServer.port()
+        WireMock.configureFor("localhost", port)
+    }
+
+    def cleanup() {
+        WireMock.reset()
+    }
+
+    def 'should handle exception thrown in getBody() during error handling when body empty'() {
+        given:
+            def template = new RestTemplate()
+        and:
+            returns404withNoBody("/bad")
+
+        when:
+            template.getForObject("http://localhost:$port/bad", String)
+
+        then:
+            def ex = thrown(ResponseException)
+            ex.httpStatus == NOT_FOUND
+            ex.body == null
+    }
+
+    def 'should include 404 body'() {
+        given:
+            def template = new RestTemplate()
+        and:
+            returns404withBody("/bad2", "NOT_FOUND_SORRY")
+
+        when:
+            template.getForObject("http://localhost:$port/bad2", String)
+
+        then:
+            def ex = thrown(ResponseException)
+            ex.httpStatus == NOT_FOUND
+            ex.body == "NOT_FOUND_SORRY"
+    }
+
+    private static returns404withNoBody(String address) {
+        stubFor(get(urlEqualTo(address))
+                .willReturn(
+                aResponse()
+                        .withStatus(NOT_FOUND.value())))
+    }
+
+    private static returns404withBody(String address, String body) {
+        stubFor(get(urlEqualTo(address))
+                .willReturn(
+                aResponse()
+                        .withStatus(NOT_FOUND.value())
+                        .withBody(body)))
+    }
+
+}

53ca2dac1e5ded4808eb6674caa3a3fe9309fcb0
mchmielarz <michal.chmielarz@gmail.com>
2015-01-07 12:29:31 +0100

    [#133} Returning plain JSON data from the endpoint.

--- micro-infra-spring-base/src/main/groovy/com/ofg/infrastructure/healthcheck/MicroserviceConfigurationController.groovy
+++ micro-infra-spring-base/src/main/groovy/com/ofg/infrastructure/healthcheck/MicroserviceConfigurationController.groovy
@@ -1,10 +1,9 @@
 package com.ofg.infrastructure.healthcheck
 
-import com.fasterxml.jackson.annotation.JsonRawValue
+import groovy.json.JsonSlurper
 import groovy.transform.CompileStatic
 import groovy.transform.Memoized
 import org.springframework.core.io.Resource
-import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
@@ -23,31 +22,15 @@ class MicroserviceConfigurationController {
         this.microserviceConfig = microserviceConfig
     }
 
-    @RequestMapping(value = '/${endpoints.microservicejson.id:microservice.json}', method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    Json getMicroserviceConfiguration() {
-        return new Json(configurationContent())
+    @RequestMapping(value = '${endpoints.microservicejson.id:microservice.json}', method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    Object getMicroserviceConfiguration() {
+        return getContent()
     }
 
     @Memoized
-    private String configurationContent() {
-        return microserviceConfig.inputStream.text
-    }
-
-    /**
-     * Wrapper class for the content of configuration file.
-     */
-    public static class Json {
-
-        private final String configuration
-
-        public Json(String configuration) {
-            this.configuration = configuration
-        }
-
-        @JsonRawValue
-        public String configuration() {
-            return configuration
-        }
+    private Object getContent() {
+        String configurationContent = microserviceConfig.inputStream.text
+        return new JsonSlurper().parseText(configurationContent)
     }
 
 }
--- micro-infra-spring-base/src/test/groovy/com/ofg/infrastructure/healthcheck/MicroserviceConfigurationControllerSpec.groovy
+++ micro-infra-spring-base/src/test/groovy/com/ofg/infrastructure/healthcheck/MicroserviceConfigurationControllerSpec.groovy
@@ -1,7 +1,5 @@
 package com.ofg.infrastructure.healthcheck
 
-import com.google.common.base.Charsets
-import com.google.common.io.Resources
 import com.ofg.infrastructure.base.BaseConfiguration
 import com.ofg.infrastructure.base.MvcCorrelationIdSettingIntegrationSpec
 import com.ofg.infrastructure.base.ServiceDiscoveryStubbingApplicationConfiguration
@@ -13,9 +11,8 @@ import org.springframework.test.context.ContextConfiguration
 import spock.lang.Shared
 
 import static org.hamcrest.Matchers.*
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
@@ -25,15 +22,19 @@ class MicroserviceConfigurationControllerSpec extends MvcCorrelationIdSettingInt
     @Shared @ClassRule
     public ProvideSystemProperty resolverUrlPropertyIsSet = new ProvideSystemProperty('service.resolver.url', 'localhost:2185');
 
+    /**
+     * Configuration of test microservice comes from microservice.json file and its content is expected in the body content.
+     */
     def 'should provide content of configuration file'() {
-        given:
-            URL resource = Resources.getResource('microservice.json')
-            String expectedContent = Resources.toString(resource, Charsets.UTF_8)
         expect:
             mockMvc.perform(get('/microservice.json'))
-                        .andExpect(status().isOk())
-                        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                        .andExpect(jsonPath('$.configuration', is(equalToIgnoringWhiteSpace(expectedContent))))
+                    .andExpect(status().isOk())
+                    .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(jsonPath('$.pl.this', is(equalTo('foo/bar/registration'))))
+                    .andExpect(jsonPath('$.pl.dependencies.confirmation.path', is(equalTo('foo/bar/security/confirmation'))))
+                    .andExpect(jsonPath('$.pl.dependencies.foo-bar.path', is(equalTo('com/ofg/foo/bar'))))
+                    .andExpect(jsonPath('$.pl.dependencies.newsletter.path', is(equalTo('foo/bar/comms/newsletter'))))
+                    .andExpect(jsonPath('$.pl.dependencies.users.path', is(equalTo('foo/bar/users'))))
     }
 
 }

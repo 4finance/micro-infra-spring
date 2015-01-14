8a12ca6ed1ab8f80796aabd7252c023c648a79ef
Tomasz Nurkiewicz <tomasz.nurkiewicz@4finance.com>
2015-01-14 15:02:45 +0100

    Fix issues when host is a placeholder in URL With URL from template or GString if host was given in a placeholder, exception was thrown. This commit proactively replaces host placeholder with hard-coded value. Fixes #161

--- micro-infra-spring-base/src/test/groovy/com/ofg/infrastructure/web/resttemplate/fluent/ServiceRestClientIntegrationSpec.groovy
+++ micro-infra-spring-base/src/test/groovy/com/ofg/infrastructure/web/resttemplate/fluent/ServiceRestClientIntegrationSpec.groovy
@@ -1,9 +1,14 @@
 package com.ofg.infrastructure.web.resttemplate.fluent
 
+import com.google.common.base.Optional
+import com.google.common.util.concurrent.ListenableFuture
 import com.ofg.config.BasicProfiles
 import com.ofg.infrastructure.base.BaseConfiguration
 import com.ofg.infrastructure.base.MvcWiremockIntegrationSpec
 import com.ofg.infrastructure.base.ServiceDiscoveryStubbingApplicationConfiguration
+import com.ofg.infrastructure.discovery.ServiceAlias
+import com.ofg.infrastructure.discovery.ServicePath
+import com.ofg.infrastructure.discovery.ServiceResolver
 import org.junit.ClassRule
 import org.junit.contrib.java.lang.system.ProvideSystemProperty
 import org.springframework.beans.factory.annotation.Autowired
@@ -11,6 +16,7 @@ import org.springframework.boot.test.SpringApplicationContextLoader
 import org.springframework.http.ResponseEntity
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
+import spock.lang.IgnoreRest
 import spock.lang.Shared
 
 @ActiveProfiles(['stub', BasicProfiles.TEST])
@@ -26,6 +32,8 @@ class ServiceRestClientIntegrationSpec extends MvcWiremockIntegrationSpec {
 
     @Autowired ServiceRestClient serviceRestClient
 
+    @Autowired ServiceResolver serviceResolver
+
     def "should send a request to provided URL with appending host when calling service"() {
         when:
             ResponseEntity<String> result = serviceRestClient
@@ -38,5 +46,60 @@ class ServiceRestClientIntegrationSpec extends MvcWiremockIntegrationSpec {
         then:
             result.body == CONTEXT_SPECIFIC_FOOBAR
     }
-    
+
+    def "should properly construct parameterized external URL"() {
+        given:
+            ServicePath path = serviceResolver.resolveAlias(new ServiceAlias(COLLABORATOR_NAME))
+            URI uri = serviceResolver.getUri(path).get()
+        when:
+            String result = serviceRestClient
+                    .forExternalService()
+                    .get()
+                    .onUrlFromTemplate(uri.toString() + '/pl/{name}')
+                    .withVariables('foobar')
+                    .andExecuteFor()
+                    .anObject()
+                    .ofType(String)
+        then:
+            result == CONTEXT_SPECIFIC_FOOBAR
+    }
+
+    def "should properly construct external URL from template"() {
+        given:
+            ServicePath path = serviceResolver.resolveAlias(new ServiceAlias(COLLABORATOR_NAME))
+            String uri = serviceResolver.getUri(path).get().toString()
+        when:
+            String result = serviceRestClient
+                    .forExternalService()
+                    .get()
+                    .onUrlFromTemplate('{uri}/pl/{name}')
+                    .withVariables(uri, 'foobar')
+                    .andExecuteFor()
+                    .anObject()
+                    .ofType(String)
+        then:
+            result == CONTEXT_SPECIFIC_FOOBAR
+    }
+
+    def "should properly construct external URL from GString"() {
+        given:
+            URI uri = uriOf(COLLABORATOR_NAME)
+            String name = 'foobar'
+        when:
+            String result = serviceRestClient
+                    .forExternalService()
+                    .get()
+                    .onUrl("${uri.toString()}/pl/$name")
+                    .andExecuteFor()
+                    .anObject()
+                    .ofType(String)
+        then:
+            result == CONTEXT_SPECIFIC_FOOBAR
+    }
+
+    private URI uriOf(String collaboratorAlias) {
+        ServicePath path = serviceResolver.resolveAlias(new ServiceAlias(COLLABORATOR_NAME))
+        return serviceResolver.getUri(path).get()
+    }
+
 }

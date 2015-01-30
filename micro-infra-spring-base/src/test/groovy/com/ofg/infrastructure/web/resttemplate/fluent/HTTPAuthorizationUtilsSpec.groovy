f25e2d03ab33556fd01333e4c22ef13eeca950cb
Tomasz Nurkiewicz <tomasz.nurkiewicz@4finance.com>
2015-01-30 16:57:30 +0100

    Moved test to appropriate class

---
+++ micro-infra-spring-base/src/test/groovy/com/ofg/infrastructure/web/resttemplate/fluent/HTTPAuthorizationUtilsSpec.groovy
@@ -0,0 +1,22 @@
+package com.ofg.infrastructure.web.resttemplate.fluent
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import static com.ofg.infrastructure.web.resttemplate.fluent.HTTPAuthorizationUtils.encodeCredentials
+
+@Unroll
+class HTTPAuthorizationUtilsSpec extends Specification {
+
+    def "encode basic authentication #username and #password credentials into #authorizationValue"() {
+        when:
+            def encodedResult = encodeCredentials(username, password)
+        then:
+            encodedResult == authorizationValue
+        where:
+            username  | password      || authorizationValue
+            'Aladdin' | 'open sesame' || 'QWxhZGRpbjpvcGVuIHNlc2FtZQ=='
+            'Denis'   | "Denis123"    || 'RGVuaXM6RGVuaXMxMjM='
+    }
+
+}

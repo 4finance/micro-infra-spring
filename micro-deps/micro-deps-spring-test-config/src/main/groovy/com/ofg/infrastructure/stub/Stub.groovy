fdea969a73df160554bf23b7f40a3a9de8bc4433
mchmielarz <michal.chmielarz@gmail.com>
2015-02-03 16:44:07 +0100

    Added javadoc to Stub methods.

--- micro-deps/micro-deps-spring-test-config/src/main/groovy/com/ofg/infrastructure/stub/Stub.groovy
+++ micro-deps/micro-deps-spring-test-config/src/main/groovy/com/ofg/infrastructure/stub/Stub.groovy
@@ -14,14 +14,29 @@ class Stub {
         this.delegate = new WireMock(host, port)
     }
 
+    /**
+     * Verifies that a single call to the mock with given expected request criteria took place.
+     *
+     * @param requestPatternBuilder expected request criteria
+     */
     void verifyThat(RequestPatternBuilder requestPatternBuilder) {
         delegate.verifyThat(requestPatternBuilder)
     }
 
+    /**
+     * Verifies that multiple calls ({@code count} times) to the mock with given expected request criteria took place.
+     *
+     * @param count expectation on how many times the interaction with mock took place
+     * @param requestPatternBuilder expected request criteria
+     */
     void verifyThat(int count, RequestPatternBuilder requestPatternBuilder) {
         delegate.verifyThat(count, requestPatternBuilder)
     }
 
+    /**
+     * Resets all interactions and mappings for this stub to the starting point,
+     * i.e. with no interactions registered and mappings loaded from stub mappings definitions.
+     */
     void resetToDefaults() {
         def stubMappings = delegate.allStubMappings()
         delegate.resetToDefaultMappings()

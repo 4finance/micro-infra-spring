a2feddf8c5a9216d36309369dc5fd0b372a7426d
Marcin Zajaczkowski <marcin.zajaczkowski@4finance.com>
2015-02-27 19:07:12 +0100

    Upgrade Spring Boot to 1.2.2 and Hystrix to 1.4.0-rc.9
    Also Spring Framework upgraded to 4.1.5 (needed by Spring Boot).

--- micro-infra-spring-base/src/test/groovy/com/ofg/infrastructure/hystrix/CircuitBreakers.groovy
+++ micro-infra-spring-base/src/test/groovy/com/ofg/infrastructure/hystrix/CircuitBreakers.groovy
@@ -2,16 +2,19 @@ package com.ofg.infrastructure.hystrix
 
 import com.netflix.hystrix.HystrixCommand
 import com.netflix.hystrix.HystrixCommandProperties
+import groovy.transform.CompileStatic
 
 import static com.netflix.hystrix.HystrixCommand.Setter.withGroupKey
 import static com.netflix.hystrix.HystrixCommandGroupKey.Factory.asKey
 
+@CompileStatic
 class CircuitBreakers {
 
     static HystrixCommand.Setter anyWithTimeout(int timeoutInMillis) {
         //below workaround comes from https://jira.codehaus.org/browse/GROOVY-6286
-        HystrixCommandProperties.Setter commandPropertiesSetter = HystrixCommandProperties.invokeMethod("Setter", null)
-        return any().andCommandPropertiesDefaults(commandPropertiesSetter.withTimeoutInMilliseconds(timeoutInMillis))
+        HystrixCommandProperties.Setter commandPropertiesSetter =
+                (HystrixCommandProperties.Setter)HystrixCommandProperties.invokeMethod("Setter", null)
+        return any().andCommandPropertiesDefaults(commandPropertiesSetter.withExecutionTimeoutInMilliseconds(timeoutInMillis))
     }
 
     static HystrixCommand.Setter any() {

8a5d70de5c47cee7b01717d22ea4bbfe7c0bcca8
konrad.dobrzynski <konrad.dobrzynski@4finance.com>
2015-04-02 09:38:08 +0200

    Spring configurer likewise way to configure ServiceRestClient

--- micro-infra-spring-base/src/main/groovy/com/ofg/infrastructure/web/resttemplate/fluent/config/ServiceRestClientConfigurer.groovy
+++ micro-infra-spring-base/src/main/groovy/com/ofg/infrastructure/web/resttemplate/fluent/config/ServiceRestClientConfigurer.groovy
@@ -1,22 +1,28 @@
 package com.ofg.infrastructure.web.resttemplate.fluent.config
-
-import org.springframework.web.client.RestOperations
-
+import org.springframework.http.converter.HttpMessageConverter
 /**
- * Interface to be implemented by Spring beans willing to provide their own components for
+ * Interface to be implemented by Spring beans willing to provide their own configuration for
  * {@link com.ofg.infrastructure.web.resttemplate.fluent.ServiceRestClientConfiguration ServiceRestClientConfiguration}.
  *
- * <p>Consider using {@link com.ofg.infrastructure.web.resttemplate.fluent.config.ServiceRestClientConfigurerSupport ServiceRestClientConfigurerSupport}
- * providing default implementations for all components. Only custom components needs to be overridden. Furthermore, backward compatibility
+ * <p>Consider using {@link com.ofg.infrastructure.web.resttemplate.fluent.config.ServiceRestClientConfigurerAdapter ServiceRestClientConfigurerAdapter}
+ * providing default implementations for all components. Only custom configuration needs to be overridden. Furthermore, backward compatibility
  * of this interface will be insured in case new customization options are introduced in the future.
  *
  * <p>See {@link com.ofg.infrastructure.web.resttemplate.fluent.ServiceRestClientConfiguration ServiceRestClientConfiguration}.
  *
- * @since 0.8.3
+ * @since 0.8.15
  */
 interface ServiceRestClientConfigurer {
 
-    RestOperations getRestTemplate()
+    /**
+     * Configures underlaying message converters
+     * @param converters
+     */
+    void configureMessageConverters(List<HttpMessageConverter<?>> converters);
 
-    //Other important beans...
+    /**
+     *
+     * @param configurer
+     */
+    void configureRestClientParams(RestClientConfigurer configurer);
 }

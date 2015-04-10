8bd32d42686903513656f890c6e5ac8b5cb20299
urszula.choromanska <urszula.choromanska@4finance.com>
2015-04-10 16:07:57 +0200

    micro-infra-spring-base hystrix rewrite groovy to java

---
+++ micro-infra-spring-base/src/main/java/com/ofg/infrastructure/hystrix/HystrixServletConfiguration.java
@@ -0,0 +1,22 @@
+package com.ofg.infrastructure.hystrix;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Registers a servlet that will be streaming Hystrix data
+ *
+ * @see HystrixMetricsStreamServlet
+ */
+@Configuration
+@Conditional(IsHystrixServletEnabled.class)
+public class HystrixServletConfiguration {
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean(@Value("${hystrix.stream-servlet.path:/health/hystrix.stream}") String hystrixMetricsStreamPath) {
+        return new ServletRegistrationBean(new HystrixMetricsStreamServlet(), hystrixMetricsStreamPath);
+    }
+}

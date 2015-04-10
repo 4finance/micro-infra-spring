7e4a60cd6ec2efd3a912d125a2aca1c89227431e
Rafal Kawecki <Rafal.Kawecki@4finance.com>
2015-04-10 12:33:50 +0200

    Discovery package moved to java

---
+++ micro-infra-spring-base/src/main/java/com/ofg/infrastructure/discovery/ServiceDiscoveryConfiguration.java
@@ -0,0 +1,16 @@
+package com.ofg.infrastructure.discovery;
+
+import groovy.transform.CompileStatic;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+/**
+ * This configuration imports configurations related to service discovery
+ *
+ * @see ServiceResolverConfiguration
+ */
+@CompileStatic
+@Configuration
+@Import(ServiceResolverConfiguration.class)
+public class ServiceDiscoveryConfiguration {
+}

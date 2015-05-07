d8f32bb033b9669be85e2f2bfd82dad11479507b
Marcin Grzejszczak <marcin@grzejszczak.pl>
2015-05-07 09:11:34 +0200

    Converted Elvises to Java representatives

--- micro-deps/micro-deps/src/main/groovy/com/ofg/infrastructure/discovery/util/DependencyCreator.groovy
+++ micro-deps/micro-deps/src/main/groovy/com/ofg/infrastructure/discovery/util/DependencyCreator.groovy
@@ -1,20 +1,21 @@
 package com.ofg.infrastructure.discovery.util
-
+import com.google.common.base.MoreObjects
 import com.ofg.infrastructure.discovery.MicroserviceConfiguration
 import com.ofg.infrastructure.discovery.ServiceAlias
 import com.ofg.infrastructure.discovery.ServicePath
+import org.apache.commons.lang.StringUtils
 
 class DependencyCreator {
 
-    static List<MicroserviceConfiguration.Dependency> fromMap(Map<String, Map<String, String>> dependency) {
-        return dependency.collect { String key, Map<String, String> value ->
+    static List<MicroserviceConfiguration.Dependency> fromMap(Map<String, Map<String, Object>> dependency) {
+        return dependency.collect { String key, Map<String, Object> value ->
             new MicroserviceConfiguration.Dependency(new ServiceAlias(key),
-                    new ServicePath(value['path']),
-                    value['required'] as Boolean ?: false,
-                    value['load-balancer-type'] as LoadBalancerType ?: LoadBalancerType.ROUND_ROBIN,
-                    value['contentTypeTemplate'] ?: '',
-                    value['version'] ?: '',
-                    value['headers'] as Map<String, String> ?: [:])
+                    new ServicePath((String)value['path']),
+                    Boolean.valueOf((String)value['required']),
+                    LoadBalancerType.fromName((String)value['load-balancer-type']),
+                    MoreObjects.firstNonNull((String)value['contentTypeTemplate'], StringUtils.EMPTY),
+                    MoreObjects.firstNonNull((String)value['version'], StringUtils.EMPTY),
+                    MoreObjects.firstNonNull((Map<String, String>)value['headers'], new HashMap<String, String>()))
         }
     }
 }

527bbe0e4392c3f8be059b78125ac66ada216cf8
Marcin Grzejszczak <marcin@grzejszczak.pl>
2015-05-10 18:36:50 +0200

    Migrating Camel to JDK7

---
+++ micro-infra-camel/src/main/java/com/ofg/infrastructure/camel/CorrelationIdInterceptor.java
@@ -0,0 +1,59 @@
+package com.ofg.infrastructure.camel;
+
+import com.ofg.infrastructure.correlationid.CorrelationIdUpdater;
+import com.ofg.infrastructure.correlationid.UuidGenerator;
+import org.apache.camel.Exchange;
+import org.apache.camel.Message;
+import org.apache.camel.Processor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.invoke.MethodHandles;
+
+import static com.ofg.infrastructure.correlationid.CorrelationIdHolder.CORRELATION_ID_HEADER;
+
+/**
+ * Interceptor class that ensures the correlationId header is present in {@Exchange}.
+ */
+public class CorrelationIdInterceptor implements Processor {
+
+    private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    private final UuidGenerator uuidGenerator;
+
+    public CorrelationIdInterceptor(UuidGenerator uuidGenerator) {
+        this.uuidGenerator = uuidGenerator;
+    }
+
+    /**
+     * Ensures correlationId header is set in incoming message (if is missing a new correlationId is created and set).
+     *
+     * @param exchange Camel's container holding received message
+     * @throws Exception if an internal processing error has occurred
+     */
+    @Override
+    public void process(Exchange exchange) throws Exception {
+        String correlationIdHeader = getCorrelationId(exchange);
+        CorrelationIdUpdater.updateCorrelationId(correlationIdHeader);
+        setCorrelationIdHeaderIfMissing(exchange, correlationIdHeader);
+    }
+
+    private String getCorrelationId(Exchange exchange) {
+        String correlationIdHeader = (String) exchange.getIn().getHeader(CORRELATION_ID_HEADER);
+        if (correlationIdHeader == null) {
+            log.debug("No correlationId has been set in request inbound message. Creating new one.");
+            correlationIdHeader = uuidGenerator.create();
+        }
+
+        return correlationIdHeader;
+    }
+
+    private void setCorrelationIdHeaderIfMissing(Exchange exchange, String correlationIdHeader) {
+        Message inboundMessage = exchange.getIn();
+        if (!inboundMessage.getHeaders().containsKey(CORRELATION_ID_HEADER)) {
+            log.debug("Setting correlationId [" + correlationIdHeader + "] in header of inbound message");
+            inboundMessage.setHeader(CORRELATION_ID_HEADER, correlationIdHeader);
+        }
+
+    }
+}
---
+++ micro-infra-camel/src/main/java/com/ofg/infrastructure/camel/aspects/CorrelationIdOnCamelRouteAspect.java
@@ -0,0 +1,38 @@
+package com.ofg.infrastructure.camel.aspects;
+
+import com.ofg.infrastructure.camel.CorrelationIdInterceptor;
+import com.ofg.infrastructure.correlationid.UuidGenerator;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.invoke.MethodHandles;
+
+/**
+ * Aspect that adds {@link CorrelationIdInterceptor} to {@link RouteBuilder} for all incoming messages
+ * and every message send to any endpoint.
+ */
+@Aspect
+public class CorrelationIdOnCamelRouteAspect {
+    private static final String ANY = "*";
+
+    private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    private final Processor correlationIdInterceptor;
+
+    public CorrelationIdOnCamelRouteAspect(UuidGenerator uuidGenerator) {
+        correlationIdInterceptor = new CorrelationIdInterceptor(uuidGenerator);
+    }
+    @Around(value = "execution(* org.apache.camel.builder.RouteBuilder.addRoutesToCamelContext(..))")
+    public Object aroundAddRoutesNoArgs(ProceedingJoinPoint joinPoint) throws Throwable {
+        final RouteBuilder targetRouteBuilder = (RouteBuilder) joinPoint.getTarget();
+        log.debug("Setting correlationId interception on " + targetRouteBuilder.getClass().getName());
+        targetRouteBuilder.interceptFrom().process(correlationIdInterceptor);
+        targetRouteBuilder.interceptSendToEndpoint(ANY).process(correlationIdInterceptor);
+        return joinPoint.proceed();
+    }
+}

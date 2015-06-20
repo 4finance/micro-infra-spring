f69c101b88cf35876517d880ad1af23f179cffa5
Marcin Grzejszczak <marcin.grzejszczak@4finance.com>
2015-06-20 21:06:44 +0200

    Revert "[#441] Removed deprecated API"
    This reverts commit b1dac605a0495e0a9b6302146d8b611bae989012.

--- stub-runner/stub-runner-spring/src/main/groovy/com/ofg/stub/config/StubRunnerConfiguration.groovy
+++ stub-runner/stub-runner-spring/src/main/groovy/com/ofg/stub/config/StubRunnerConfiguration.groovy
@@ -12,10 +12,24 @@ import org.springframework.context.annotation.Import
 /**
  * Configuration that initializes a {@link BatchStubRunner} that runs {@link StubRunner} instance for each microservice's collaborator.
  *
+ * Properties that can be set are
+ *
+ * <ul>
+ *     <li>{@code stubrunner.port.range.min} default value {@code 10000} - minimal port value for the collaborator's Wiremock</li>
+ *     <li>{@code stubrunner.port.range.max} default value {@code 15000} - maximal port value for the collaborator's Wiremock</li>
+ *     <li>{@code stubrunner.stubs.repository.root} default value {@code 4finance Nexus Repository location} - point to where your stub mappings are uploaded as a jar</li>
+ *     <li>{@code stubrunner.stubs.group} default value {@code com.ofg} - group name of your stub mappings</li>
+ *     <li>{@code stubrunner.stubs.module} default value {@code stub-definitions} - artifactId of your stub mappings</li>
+ *     <li>{@code stubrunner.skip-local-repo} default value {@code false} - avoids local repository in the dependency resolution & always pulls from the remote</li>
+ *     <li>{@code stubrunner.use-microservice-definitions} default value {@code false} - use per microservice stub resolution rather than one jar for all microservices</li>
+ *     <li>{@code stubrunner.wait-for-service} default value {@code false} - wait for connection from the service to local Zookeeper</li>
+ *     <li>{@code stubrunner.wait-timeout} default value {@code 1} - sets wait-for-service timeout</li>
+ * </ul>
+ *
  * What happens under the hood is that
  *
  * <ul>
- *     <li>Depending on value of offline work parameter {@link Grape} takes the latest version of stub mappings from local repository or downloads it from remote one</li>
+ *     <li>Depending on value of {@code stubrunner.use.local.repo} parameter {@link Grape} takes the latest version of stub mappings from local repository or downloads it from remote one</li>
  *     <li>The dependency is unpacked to a temporary folder</li>
  *     <li>Basing on your {@code microservice.json} setup, for each collaborator a {@link StubRunner} is set up</li>
  *     <li>Each {@link StubRunner} is initialized with a single instance of the Zookeeper {@link TestingServer}</li>
@@ -44,6 +58,7 @@ class StubRunnerConfiguration {
      * @param @Deprecated stubsModule module name of the dependency containing stub mappings
      * @param stubsSuffix suffix for the jar containing stubs
      * @param workOffline forces offline work
+     * @param @Deprecated skipLocalRepo avoids local repository in dependency resolution
      * @param useMicroserviceDefinitions use per microservice stub resolution rather than one jar for all microservices
      * @param waitForService wait for connection from service
      * @param waitTimeout wait timeout in seconds
@@ -58,16 +73,22 @@ class StubRunnerConfiguration {
                                 @Deprecated @Value('${stubrunner.stubs.module:stub-definitions}') String stubsModule,
                                 @Value('${stubrunner.stubs.suffix:stubs}') String stubsSuffx,
                                 @Value('${stubrunner.work-offline:false}') boolean workOffline,
+                                @Deprecated @Value('${stubrunner.skip-local-repo:true}') boolean skipLocalRepo,
                                 @Value('${stubrunner.use-microservice-definitions:false}') boolean useMicroserviceDefinitions,
                                 @Value('${stubrunner.wait-for-service:false}') boolean waitForService,
                                 @Value('${stubrunner.wait-timeout:1}') Integer waitTimeout,
                                 TestingServer testingServer,
                                 ServiceConfigurationResolver serviceConfigurationResolver) {
-        StubRunnerOptions stubRunnerOptions = new StubRunnerOptions(minPortValue, maxPortValue, stubRepositoryRoot, stubsGroup, stubsModule, workOffline,
+        boolean shouldWorkOnline = isPropertySetToWorkOnline(workOffline, skipLocalRepo)
+        StubRunnerOptions stubRunnerOptions = new StubRunnerOptions(minPortValue, maxPortValue, stubRepositoryRoot, stubsGroup, stubsModule, shouldWorkOnline,
                 useMicroserviceDefinitions, testingServer.connectString, testingServer.port, stubsSuffx, waitForService, waitTimeout)
         List<String> dependenciesPath = serviceConfigurationResolver.dependencies.collect { it.servicePath.path }
         Collaborators dependencies = new Collaborators(serviceConfigurationResolver.basePath, dependenciesPath)
         return new BatchStubRunnerFactory(stubRunnerOptions, dependencies).buildBatchStubRunner()
+    }
+
+    private boolean isPropertySetToWorkOnline(boolean workOffline, boolean skipLocalRepo) {
+        return workOffline ? false : skipLocalRepo
     }
 
 }

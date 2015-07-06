4finance/micro-infra-spring

commit 0ec2c3fc273c454d00be9bc574019dcbcd5ac537

CorrelationId filters and exceptions

micro-infra-spring-base/src/main/groovy/com/ofg/infrastructure/web/correlationid/CorrelationIdConfiguration.java
@@ -1,0 +1,39 @@
+package com.ofg.infrastructure.web.correlationid;
+
+import com.ofg.infrastructure.correlationid.UuidGenerator;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.regex.Pattern;
+
+/**
+ * Registers beans that add correlation id to requests
+ *
+ * @see CorrelationIdAspect
+ * @see CorrelationIdFilter
+ */
+@Configuration
+public class CorrelationIdConfiguration {
+
+    @Value("${rest.correlationId.skipPattern:}")
+    private String skipPattern;
+
+    @Bean
+    public CorrelationIdAspect correlationIdAspect() {
+        return new CorrelationIdAspect();
+    }
+
+    @Bean
+    public FilterRegistrationBean correlationHeaderFilter(UuidGenerator uuidGenerator) {
+        Pattern pattern = StringUtils.isBlank(skipPattern) ? Pattern.compile(skipPattern) : CorrelationIdFilter.DEFAULT_SKIP_PATTERN;
+        return new FilterRegistrationBean(new CorrelationIdFilter(uuidGenerator, pattern));
+    }
+
+    @Bean
+    public UuidGenerator uuidGenerator() {
+        return new UuidGenerator();
+    }
+}

micro-deps/micro-deps-spring-config/src/main/java/com/ofg/infrastructure/discovery/ServiceDiscoveryInfrastructureConfiguration.java
@@ -87,6 +87,7 @@ public class ServiceDiscoveryInfrastructureConfiguration {
     }
 
     @Bean(initMethod = "start", destroyMethod = "close")
+    @SuppressWarnings("unchecked")
     public ServiceDiscovery serviceDiscovery(CuratorFramework curatorFramework,
                                              ServiceInstance serviceInstance,
                                              ServiceConfigurationResolver serviceConfigurationResolver) {

micro-infra-spring-base/src/main/groovy/com/ofg/infrastructure/web/correlationid/CorrelationIdConfiguration.groovy
@@ -1,40 +1,0 @@
-package com.ofg.infrastructure.web.correlationid
-
-import com.ofg.infrastructure.correlationid.UuidGenerator
-import groovy.transform.CompileStatic
-import org.apache.commons.lang.StringUtils
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.boot.context.embedded.FilterRegistrationBean
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-
-import java.util.regex.Pattern
-
-/**
- * Registers beans that add correlation id to requests
- * 
- * @see CorrelationIdAspect
- * @see CorrelationIdFilter
- */
-@Configuration
-@CompileStatic
-class CorrelationIdConfiguration {
-
-    @Bean
-    CorrelationIdAspect correlationIdAspect() {
-        return new CorrelationIdAspect()
-    }
-
-    @Bean
-    FilterRegistrationBean correlationHeaderFilter(UuidGenerator uuidGenerator, @Value('${rest.correlationId.skipPattern:}') String skipPattern) {
-        Pattern pattern = StringUtils.isNotBlank(skipPattern)?
-                Pattern.compile(skipPattern) :
-                CorrelationIdFilter.DEFAULT_SKIP_PATTERN
-        return new FilterRegistrationBean(new CorrelationIdFilter(uuidGenerator, pattern))
-    }
-
-    @Bean
-    UuidGenerator uuidGenerator() {
-        return new UuidGenerator()
-    }
-}

micro-infra-spring-base/src/main/groovy/com/ofg/infrastructure/web/correlationid/CorrelationIdFilter.groovy
@@ -1,117 +1,0 @@
-package com.ofg.infrastructure.web.correlationid
-
-import com.google.common.base.Function
-import com.google.common.base.Optional as GOptional
-import com.ofg.infrastructure.correlationid.CorrelationIdHolder
-import com.ofg.infrastructure.correlationid.UuidGenerator
-import groovy.transform.CompileStatic
-import groovy.util.logging.Slf4j
-import org.slf4j.MDC
-import org.springframework.web.filter.OncePerRequestFilter
-
-import javax.servlet.FilterChain
-import javax.servlet.ServletException
-import javax.servlet.http.HttpServletRequest
-import javax.servlet.http.HttpServletResponse
-import java.util.regex.Pattern
-
-import static com.ofg.infrastructure.correlationid.CorrelationIdHolder.CORRELATION_ID_HEADER
-import static org.springframework.util.StringUtils.hasText
-
-/**
- * Filter that takes the value of the {@link com.ofg.infrastructure.correlationid.CorrelationIdHolder#CORRELATION_ID_HEADER} header
- * from either request or response and sets it in the {@link com.ofg.infrastructure.correlationid.CorrelationIdHolder}. It also provides
- * that value in {@link org.slf4j.MDC} logging related class so that logger prints the value of
- * correlation id at each log.
- * 
- * @see com.ofg.infrastructure.correlationid.CorrelationIdHolder
- * @see org.slf4j.MDC
- */
-@Slf4j
-@CompileStatic
-//inspired by http://taidevcouk.wordpress.com/2014/05/26/implementing-correlation-ids-in-spring-boot/
-class CorrelationIdFilter extends OncePerRequestFilter {
-
-    public static final Pattern DEFAULT_SKIP_PATTERN =
-            ~/\/api-docs.*|\/autoconfig|\/configprops|\/dump|\/info|\/metrics.*|\/mappings|\/trace|\/swagger.*|.*\.png|.*\.css|.*\.js|.*\.html/
-
-    private final GOptional<Pattern> skipCorrId
-    private final UuidGenerator uuidGenerator
-
-    CorrelationIdFilter() {
-        this.uuidGenerator = new UuidGenerator()
-        this.skipCorrId = GOptional.absent()
-    }
-
-    CorrelationIdFilter(UuidGenerator uuidGenerator, Pattern skipCorrId) {
-        this.uuidGenerator = uuidGenerator
-        this.skipCorrId = GOptional.of(skipCorrId)
-    }
-
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        setupCorrelationId(request, response)
-        try {
-            filterChain.doFilter(request, response)
-        } finally {
-            cleanupCorrelationId()
-        }
-    }
-
-    private void setupCorrelationId(HttpServletRequest request, HttpServletResponse response) {
-        String correlationId = getCorrelationIdFrom(request) ?: getCorrelationIdFrom(response)
-        if(!hasText(correlationId) && shouldGenerateCorrId(request)) {
-            correlationId = createNewCorrIdIfEmpty()
-        }
-        CorrelationIdHolder.set(correlationId)
-        addCorrelationIdToResponseIfNotPresent(response, correlationId)
-    }
-
-    private String getCorrelationIdFrom(HttpServletResponse response) {
-        return withLoggingAs('response') { response.getHeader(CORRELATION_ID_HEADER) }
-    }
-
-    private String getCorrelationIdFrom(HttpServletRequest request) {
-        return withLoggingAs('request') { request.getHeader(CORRELATION_ID_HEADER) }
-    }
-
-    private withLoggingAs(String whereWasFound, Closure correlationIdGetter) {
-        String correlationId = correlationIdGetter.call()
-        if (hasText(correlationId)) {
-            MDC.put(CORRELATION_ID_HEADER, correlationId)
-            log.debug("Found correlationId in $whereWasFound: $correlationId")
-        }
-        return correlationId
-    }
-
-    private String createNewCorrIdIfEmpty() {
-        String currentCorrId = uuidGenerator.create()
-        MDC.put(CORRELATION_ID_HEADER, currentCorrId)
-        log.debug("Generating new correlationId: " + currentCorrId)
-        return currentCorrId
-    }
-
-    protected boolean shouldGenerateCorrId(HttpServletRequest request) {
-        String uri = request.requestURI?: ''
-        boolean skip = skipCorrId
-                .transform({Pattern skipPattern -> uri.matches(skipPattern)} as Function<Pattern, Boolean>)
-                .or(false)
-        return !skip
-    }
-
-    private void addCorrelationIdToResponseIfNotPresent(HttpServletResponse response, String correlationId) {
-        if (!hasText(response.getHeader(CORRELATION_ID_HEADER))) {
-            response.addHeader(CORRELATION_ID_HEADER, correlationId)
-        }
-    }
-
-    private void cleanupCorrelationId() {
-        MDC.remove(CORRELATION_ID_HEADER)
-        CorrelationIdHolder.remove()
-    }
-
-    @Override
-    protected boolean shouldNotFilterAsyncDispatch() {
-        return false
-    }
-}

micro-infra-spring-base/src/main/groovy/com/ofg/infrastructure/web/correlationid/CorrelationIdFilter.java
@@ -1,0 +1,142 @@
+package com.ofg.infrastructure.web.correlationid;
+
+import com.google.common.base.Function;
+import com.google.common.base.Optional;
+import com.ofg.infrastructure.correlationid.CorrelationIdHolder;
+import com.ofg.infrastructure.correlationid.UuidGenerator;
+import groovy.transform.CompileStatic;
+import org.codehaus.groovy.runtime.StringGroovyMethods;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.lang.invoke.MethodHandles;
+import java.util.concurrent.Callable;
+import java.util.regex.Pattern;
+
+import static com.ofg.infrastructure.correlationid.CorrelationIdHolder.CORRELATION_ID_HEADER;
+import static org.springframework.util.StringUtils.hasText;
+
+/**
+ * Filter that takes the value of the {@link CorrelationIdHolder#CORRELATION_ID_HEADER} header
+ * from either request or response and sets it in the {@link CorrelationIdHolder}. It also provides
+ * that value in {@link MDC} logging related class so that logger prints the value of
+ * correlation id at each log.
+ *
+ * @see CorrelationIdHolder
+ * @see MDC
+ */
+@CompileStatic
+public class CorrelationIdFilter extends OncePerRequestFilter {
+    private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+    public static final Pattern DEFAULT_SKIP_PATTERN = Pattern.compile("/api-docs.*|/autoconfig|/configprops|/dump|/info|/metrics.*|/mappings|/trace|/swagger.*|.*\\.png|.*\\.css|.*\\.js|.*\\.html");
+
+    private final Optional<Pattern> skipCorrId;
+    private final UuidGenerator uuidGenerator;
+
+    public CorrelationIdFilter() {
+        this.uuidGenerator = new UuidGenerator();
+        this.skipCorrId = Optional.absent();
+    }
+
+    public CorrelationIdFilter(UuidGenerator uuidGenerator, Pattern skipCorrId) {
+        this.uuidGenerator = uuidGenerator;
+        this.skipCorrId = Optional.of(skipCorrId);
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        setupCorrelationId(request, response);
+        try {
+            filterChain.doFilter(request, response);
+        } finally {
+            cleanupCorrelationId();
+        }
+    }
+
+    private void setupCorrelationId(HttpServletRequest request, HttpServletResponse response) {
+        String correlationIdFromRequest = getCorrelationIdFrom(request);
+        String correlationId = (hasText(correlationIdFromRequest)) ? correlationIdFromRequest : getCorrelationIdFrom(response);
+        if (!hasText(correlationId) && shouldGenerateCorrId(request)) {
+            correlationId = createNewCorrIdIfEmpty();
+        }
+        CorrelationIdHolder.set(correlationId);
+        addCorrelationIdToResponseIfNotPresent(response, correlationId);
+    }
+
+    private String getCorrelationIdFrom(final HttpServletResponse response) {
+        return withLoggingAs("response", new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                return response.getHeader(CORRELATION_ID_HEADER);
+            }
+        });
+    }
+
+    private String getCorrelationIdFrom(final HttpServletRequest request) {
+        return withLoggingAs("request", new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                return request.getHeader(CORRELATION_ID_HEADER);
+            }
+        });
+    }
+
+    private String withLoggingAs(String whereWasFound, Callable<String> correlationIdGetter) {
+        String correlationId = tryToGetCorrelationId(correlationIdGetter);
+        if (hasText(correlationId)) {
+            MDC.put(CORRELATION_ID_HEADER, correlationId);
+            log.debug("Found correlationId in " + whereWasFound + ": " + correlationId);
+        }
+        return correlationId;
+    }
+
+    private String tryToGetCorrelationId(Callable<String> correlationIdGetter) {
+        try {
+            return correlationIdGetter.call();
+        } catch (Exception e) {
+            log.error("Exception occurred while trying to retrieve request header", e);
+            return "";
+        }
+    }
+
+    private String createNewCorrIdIfEmpty() {
+        String currentCorrId = uuidGenerator.create();
+        MDC.put(CORRELATION_ID_HEADER, currentCorrId);
+        log.debug("Generating new correlationId: " + currentCorrId);
+        return currentCorrId;
+    }
+
+    protected boolean shouldGenerateCorrId(HttpServletRequest request) {
+        final String i = request.getRequestURI();
+        final String uri = StringGroovyMethods.asBoolean(i) ? i : "";
+        boolean skip = skipCorrId.transform(new Function<Pattern, Boolean>() {
+            public Boolean apply(Pattern skipPattern) {
+                return skipPattern.matcher(uri).matches();
+            }
+        }).or(false);
+        return !skip;
+    }
+
+    private void addCorrelationIdToResponseIfNotPresent(HttpServletResponse response, String correlationId) {
+        if (!hasText(response.getHeader(CORRELATION_ID_HEADER))) {
+            response.addHeader(CORRELATION_ID_HEADER, correlationId);
+        }
+    }
+
+    private void cleanupCorrelationId() {
+        MDC.remove(CORRELATION_ID_HEADER);
+        CorrelationIdHolder.remove();
+    }
+
+    @Override
+    protected boolean shouldNotFilterAsyncDispatch() {
+        return false;
+    }
+}

micro-infra-spring-base/src/main/groovy/com/ofg/infrastructure/web/exception/BadParameterError.groovy
@@ -1,14 +1,0 @@
-package com.ofg.infrastructure.web.exception
-
-import groovy.transform.CompileStatic
-
-@CompileStatic
-class BadParameterError {
-    String field
-    String message
-
-    BadParameterError(String field, String message) {
-        this.field = field
-        this.message = message
-    }
-}

micro-infra-spring-base/src/main/groovy/com/ofg/infrastructure/web/exception/BadParameterError.java
@@ -1,0 +1,27 @@
+package com.ofg.infrastructure.web.exception;
+
+public class BadParameterError {
+    private String field;
+    private String message;
+
+    public BadParameterError(String field, String message) {
+        this.field = field;
+        this.message = message;
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}

micro-infra-spring-base/src/main/groovy/com/ofg/infrastructure/web/exception/BadParametersException.groovy
@@ -1,17 +1,0 @@
-package com.ofg.infrastructure.web.exception
-
-import groovy.transform.CompileStatic
-import org.springframework.validation.ObjectError
-
-@CompileStatic
-class BadParametersException extends RuntimeException {
-    List<ObjectError> errors
-
-    public BadParametersException(List<ObjectError> errors) {
-        this.errors = errors
-    }
-
-    public List<ObjectError> getErrors() {
-        return errors
-    }
-}

commit e2a84c62bab469d871def057996dba4c0cc9518f

Broken condition when filtering out static resource for correlation ID generation
Was fixed in https://github.com/4finance/micro-infra-spring/commit/e403c7e390c6c65646cb3ed8dfd23b2968115e8a but broken again in https://github.com/4finance/micro-infra-spring/commit/0ec2c3fc273c454d00be9bc574019dcbcd5ac537

micro-infra-spring-base/src/main/groovy/com/ofg/infrastructure/web/correlationid/CorrelationIdConfiguration.java
@@ -28,7 +28,7 @@ public class CorrelationIdConfiguration {
 
     @Bean
     public FilterRegistrationBean correlationHeaderFilter(UuidGenerator uuidGenerator) {
-        Pattern pattern = StringUtils.isBlank(skipPattern) ? Pattern.compile(skipPattern) : CorrelationIdFilter.DEFAULT_SKIP_PATTERN;
+        Pattern pattern = StringUtils.isNotBlank(skipPattern) ? Pattern.compile(skipPattern) : CorrelationIdFilter.DEFAULT_SKIP_PATTERN;
         return new FilterRegistrationBean(new CorrelationIdFilter(uuidGenerator, pattern));
     }
 
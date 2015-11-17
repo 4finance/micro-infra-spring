886ead6b4fc76f20fbe7aabb0970100497893798
Marcin Grzejszczak <marcin.grzejszczak@4finance.com>
2015-11-17 17:11:51 +0100

    Removed autoconfigurations

--- micro-infra-spring-base/src/main/groovy/com/ofg/infrastructure/web/correlationid/CorrelationIdConfiguration.java
+++ micro-infra-spring-base/src/main/groovy/com/ofg/infrastructure/web/correlationid/CorrelationIdConfiguration.java
@@ -3,10 +3,8 @@ package com.ofg.infrastructure.web.correlationid;
 import com.ofg.infrastructure.correlationid.UuidGenerator;
 import com.ofg.infrastructure.scheduling.TaskSchedulingConfiguration;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.cloud.sleuth.IdGenerator;
-import org.springframework.cloud.sleuth.instrument.web.TraceWebAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -18,7 +16,6 @@ import org.springframework.context.annotation.Import;
  */
 @Configuration
 @Import(TaskSchedulingConfiguration.class)
-@AutoConfigureBefore(TraceWebAutoConfiguration.class)
 public class CorrelationIdConfiguration {
 
     @Value("${rest.correlationId.skipPattern:}")
--- micro-infra-spring-base/src/main/java/com/ofg/infrastructure/tracing/TracingConfiguration.java
+++ micro-infra-spring-base/src/main/java/com/ofg/infrastructure/tracing/TracingConfiguration.java
@@ -1,15 +1,10 @@
 package com.ofg.infrastructure.tracing;
 
 import com.ofg.infrastructure.scheduling.TaskSchedulingConfiguration;
-import org.springframework.boot.autoconfigure.AutoConfigureBefore;
-import org.springframework.cloud.sleuth.autoconfig.TraceAutoConfiguration;
-import org.springframework.cloud.sleuth.log.SleuthLogAutoConfiguration;
-import org.springframework.cloud.sleuth.zipkin.ZipkinAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
 @Import(TaskSchedulingConfiguration.class)
-@AutoConfigureBefore({ ZipkinAutoConfiguration.class, TraceAutoConfiguration.class, SleuthLogAutoConfiguration.class })
 public class TracingConfiguration {
 }

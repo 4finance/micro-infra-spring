41884c5740dbf61050f54242355556c2697ace86
Marcin Grzejszczak <marcin.grzejszczak@gmail.com>
2015-11-20 20:40:05 +0100

    Changed branches due to backwards incompatible changes

--- gradle/runBootMicroserviceAcceptanceTests.sh
+++ gradle/runBootMicroserviceAcceptanceTests.sh
@@ -16,10 +16,11 @@ dumpCurrentMicroInfraSpringVersionToFile
 
 git clone https://github.com/4finance/boot-microservice.git
 cd boot-microservice
+git checkout tech/integration_with_sleuth
 updateMicroInfraSpringVersionInConfigurationFile
 ./gradlew clean check uptodate --stacktrace --continue --info --no-daemon
 
 git reset --hard
-git checkout boot-microservice-gui
+git checkout tech/integration_with_sleuth_gui
 updateMicroInfraSpringVersionInConfigurationFile
 ./gradlew clean check --stacktrace --continue --info --no-daemon

bc62fa653b8163bf08ad6a6f22eed03aefb30531
Marcin Zajaczkowski <marcin.zajaczkowski@4finance.com>
2015-11-23 10:35:34 +0100

    Fix UAT configuration in legacy branch

--- gradle/runBootMicroserviceAcceptanceTests.sh
+++ gradle/runBootMicroserviceAcceptanceTests.sh
@@ -16,10 +16,11 @@ dumpCurrentMicroInfraSpringVersionToFile
 
 git clone https://github.com/4finance/boot-microservice.git
 cd boot-microservice
+git checkout legacy
 updateMicroInfraSpringVersionInConfigurationFile
 ./gradlew clean check uptodate --stacktrace --continue --info --no-daemon
 
 git reset --hard
-git checkout boot-microservice-gui
+git checkout legacy-gui
 updateMicroInfraSpringVersionInConfigurationFile
 ./gradlew clean check --stacktrace --continue --info --no-daemon

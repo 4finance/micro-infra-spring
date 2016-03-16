6bfe789beb09fc8fdcc6a6377ff9de1de4617b07
Dmitry Sidorik <dmitry.sidorik@4finance.com>
2016-03-16 13:53:14 +0100

    Add JaxRsServiceResolver.getLocator(Class, ServiceAlias) method

--- micro-deps/micro-deps-jax-rs/src/main/java/com/ofg/infrastructure/jaxrs/JerseyServiceResolver.java
+++ micro-deps/micro-deps-jax-rs/src/main/java/com/ofg/infrastructure/jaxrs/JerseyServiceResolver.java
@@ -1,6 +1,8 @@
 package com.ofg.infrastructure.jaxrs;
 
+import com.google.common.base.Optional;
 import com.google.common.base.Supplier;
+import com.ofg.infrastructure.discovery.ServiceAlias;
 import com.ofg.infrastructure.discovery.ServicePath;
 import com.ofg.infrastructure.discovery.ServiceResolver;
 import org.glassfish.jersey.client.ClientConfig;
@@ -10,7 +12,6 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
 import java.net.URI;
 
 @Named
@@ -26,19 +27,29 @@ public class JerseyServiceResolver implements JaxRsServiceResolver {
 
     @Override
     public <T> Supplier<T> getLocator(final Class<T> clazz) {
-        final ServicePath path = new ServicePath(clazz.getPackage().getName().replace('.', '/'));
-        final Client client = getClient();
-        return new Supplier<T>() {
-            @Override
-            public T get() {
-                URI uri = resolver.getUri(path).get();
-                WebTarget t = client.target(uri);
-                return WebResourceFactory.newResource(clazz, t);
-            }
-        };
+        return getLocator(clazz, new ServicePath(clazz.getPackage().getName().replace('.', '/')));
+    }
+
+    @Override
+    public <T> Supplier<T> getLocator(final Class<T> clazz, final ServiceAlias alias) {
+        return getLocator(clazz, resolver.resolveAlias(alias));
     }
 
     public Client getClient() {
         return ClientBuilder.newClient(config);
+    }
+
+    private <T> Supplier<T> getLocator(final Class<T> clazz, final ServicePath path) {
+        final Client client = getClient();
+        return new Supplier<T>() {
+            @Override
+            public T get() {
+                return resolveResource(resolver.getUri(path), client, clazz);
+            }
+        };
+    }
+
+    private static <T> T resolveResource(Optional<URI> uri, Client client, Class<T> clazz) {
+        return WebResourceFactory.newResource(clazz, client.target(uri.get()));
     }
 }

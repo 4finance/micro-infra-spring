623601e2360e778ad1e0ad9b02df89986db6ca58
konrad.dobrzynski <konrad.dobrzynski@4finance.com>
2016-04-11 15:24:29 +0200

    Removed branch testing with gui -> will be removed completely

--- gradle/runBootMicroserviceAcceptanceTests.sh
+++ gradle/runBootMicroserviceAcceptanceTests.sh
@@ -17,11 +17,4 @@ dumpCurrentMicroInfraSpringVersionToFile
 git clone https://github.com/4finance/boot-microservice.git
 cd boot-microservice
 updateMicroInfraSpringVersionInConfigurationFile
-./gradlew clean check uptodate --stacktrace --continue --no-daemon
-./gradlew guiTest --stacktrace --info --continue --no-daemon
-
-git reset --hard
-git checkout boot-microservice-gui
-updateMicroInfraSpringVersionInConfigurationFile
-./gradlew clean check uptodate --stacktrace --continue --no-daemon
-./gradlew guiTest --stacktrace --info --continue --no-daemon
+./gradlew clean check guiTest uptodate --stacktrace --continue --no-daemon

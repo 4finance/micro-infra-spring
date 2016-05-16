345ddf5db038652794c1f363156cff4dcb118b99
edgars.matisans <edgars.matisans@4finance.com>
2016-05-16 14:24:18 +0300

    Resolve service localhost address

--- stub-runner/stub-runner/src/main/groovy/com/ofg/stub/registry/StubRegistry.groovy
+++ stub-runner/stub-runner/src/main/groovy/com/ofg/stub/registry/StubRegistry.groovy
@@ -52,10 +52,19 @@ class StubRegistry {
     private static ServiceInstance serviceInstanceOf(StubServer stubServer) {
         return ServiceInstance.builder()
                 .uriSpec(URI_SPEC)
-                .address('localhost')
+                .address(resolveLocalhost())
                 .port(stubServer.port)
                 .name(serviceName(stubServer))
                 .build()
+    }
+
+    private static String resolveLocalhost() {
+        try {
+            return InetAddress.getLocalHost().getHostAddress()
+        } catch (UnknownHostException e) {
+            log.error('Exception occurred while trying to resolve localhost address', e)
+            return 'localhost'
+        }
     }
 
     private static String serviceName(StubServer stubServer) {

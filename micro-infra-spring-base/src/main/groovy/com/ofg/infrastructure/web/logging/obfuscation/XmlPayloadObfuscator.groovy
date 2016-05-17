b7ee6ded7522320193284022ab9b45f2a9753492
Mariusz Smykuła <mariuszs@gmail.com>
2016-05-17 12:58:22 +0200

    Merge pull request #540 from 4finance/issue/539-permature_end_of_file… (#541)
    * Merge pull request #540 from 4finance/issue/539-permature_end_of_file_obfus
    
    539 fix
    
    * formatting

--- micro-infra-spring-base/src/main/groovy/com/ofg/infrastructure/web/logging/obfuscation/XmlPayloadObfuscator.groovy
+++ micro-infra-spring-base/src/main/groovy/com/ofg/infrastructure/web/logging/obfuscation/XmlPayloadObfuscator.groovy
@@ -8,7 +8,7 @@ import org.slf4j.LoggerFactory
 import org.springframework.core.annotation.Order
 
 @Order(value = 1)
-class XmlPayloadObfuscator extends AbstractPayloadObfuscator{
+class XmlPayloadObfuscator extends AbstractPayloadObfuscator {
 
     private static final Logger log = LoggerFactory.getLogger(XmlPayloadObfuscator.class);
 
@@ -16,19 +16,27 @@ class XmlPayloadObfuscator extends AbstractPayloadObfuscator{
         super(obfuscationFieldStrategy)
     }
 
-    String cleanFieldsFromXml(String content, List<String> fields){
-        try{
+    String cleanFieldsFromXml(String content, List<String> fields) {
+        try {
             Preconditions.checkArgument(content != null, "Content is {}", content)
             def rootNode = new XmlSlurper().parseText(content)
-            rootNode.'**'.findAll { fields?.contains(it.name()) }.each {NodeChild node -> node.replaceBody(obfuscate(node.toString())) }
+            rootNode.'**'.findAll {
+                fields?.contains(it.name())
+            }.each { NodeChild node -> node.replaceBody(obfuscate(node.toString())) }
             return new StreamingMarkupBuilder().bindNode(rootNode).toString()
-        }catch (Exception ex){log.error("Error with [$content]",ex)}
+        } catch (Exception ex) {
+            log.debug("Error with [$content]", ex)
+        }
         return content
     }
 
     @Override
     String process(String content, List<String> fieldsToObfuscate) {
-        return cleanFieldsFromXml(content, fieldsToObfuscate)
+        if (content && fieldsToObfuscate) {
+            return cleanFieldsFromXml(content, fieldsToObfuscate)
+        } else {
+            return content
+        }
     }
 
     @Override

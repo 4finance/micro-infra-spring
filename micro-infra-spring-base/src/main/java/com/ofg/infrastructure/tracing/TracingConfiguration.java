668921a3f424431b307849d8115c0a8e8a13e550
konrad.dobrzynski <konrad.dobrzynski@4finance.com>
2016-06-08 16:25:33 +0200

    Added default sampling for tracing

--- micro-infra-spring-base/src/main/java/com/ofg/infrastructure/tracing/TracingConfiguration.java
+++ micro-infra-spring-base/src/main/java/com/ofg/infrastructure/tracing/TracingConfiguration.java
@@ -1,15 +1,14 @@
 package com.ofg.infrastructure.tracing;
 
-import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.cloud.sleuth.Sampler;
-import org.springframework.cloud.sleuth.autoconfig.TraceAutoConfiguration;
-import org.springframework.cloud.sleuth.instrument.web.TraceWebAutoConfiguration;
 import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 
 @Configuration
-@AutoConfigureBefore({TraceAutoConfiguration.class, TraceWebAutoConfiguration.class})
+@AutoConfigureOrder(Ordered.HIGHEST_PRECEDENCE)
 public class TracingConfiguration {
 
     @Bean

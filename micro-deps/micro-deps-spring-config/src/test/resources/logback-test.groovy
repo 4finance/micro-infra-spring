4bead4939e3ffad9dd46efa936698594b5eccb07
Mariusz Smykula <mariuszs@gmail.com>
2016-06-15 10:34:57 +0200

    logger cleaning

--- micro-deps/micro-deps-spring-config/src/test/resources/logback-test.groovy
+++ micro-deps/micro-deps-spring-config/src/test/resources/logback-test.groovy
@@ -1,9 +1,13 @@
 import ch.qos.logback.classic.encoder.PatternLayoutEncoder
 import ch.qos.logback.core.ConsoleAppender
+import org.springframework.cloud.sleuth.Span
+
+import static ch.qos.logback.classic.Level.DEBUG
+import static ch.qos.logback.classic.Level.INFO
 
 appender("CONSOLE", ConsoleAppender) {
     encoder(PatternLayoutEncoder) {
-        pattern = "%d{HH:mm:ss.SSSZ} | %-5level | %X{X-Trace-Id} | %thread | %logger{1} | %m%n"
+        pattern = "%d{HH:mm:ss.SSSZ} | %-5level | %X{${Span.TRACE_ID_NAME}} | %thread | %logger{1} | %m%n"
     }
 }
 
--- micro-infra-spring-config/src/test/resources/logback-test.groovy
+++ micro-infra-spring-config/src/test/resources/logback-test.groovy
@@ -1,9 +1,12 @@
 import ch.qos.logback.classic.encoder.PatternLayoutEncoder
 import ch.qos.logback.core.ConsoleAppender
 
+import static ch.qos.logback.classic.Level.DEBUG
+import static ch.qos.logback.classic.Level.INFO
+
 appender("CONSOLE", ConsoleAppender) {
     encoder(PatternLayoutEncoder) {
-        pattern = "%d{HH:mm:ss.SSSZ} | %-5level | %X{X-Trace-Id} | %thread | %logger{1} | %m%n"
+        pattern = "%d{HH:mm:ss.SSSZ} | %-5level | %X{X-B3-TraceId} | %thread | %logger{1} | %m%n"
     }
 }
 

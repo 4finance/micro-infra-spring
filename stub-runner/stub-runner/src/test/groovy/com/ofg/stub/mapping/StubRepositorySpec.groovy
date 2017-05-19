14f2a69422ad8c8a29e744dd36f3457a63074cdd
Adrian Woźniak <wozniak.adrian@gmail.com>
2017-05-19 10:46:37 +0200

    Weakening subs repository layout restrictions (#615)

--- stub-runner/stub-runner/src/test/groovy/com/ofg/stub/mapping/StubRepositorySpec.groovy
+++ stub-runner/stub-runner/src/test/groovy/com/ofg/stub/mapping/StubRepositorySpec.groovy
@@ -5,23 +5,31 @@ import spock.lang.Specification
 import static com.ofg.stub.mapping.MappingDescriptor.MappingType.REALM_SPECIFIC
 
 class StubRepositorySpec extends Specification {
-    public static final File REPOSITORY_LOCATION = new File('src/test/resources/repository')
-    public static final File MAPPINGS_REPOSITORY_LOCATION = new File("$REPOSITORY_LOCATION/mappings")
-    public static final MappingDescriptor MAIN_BYE_STUB_DESCRIPTOR = new MappingDescriptor(new File(MAPPINGS_REPOSITORY_LOCATION, 'com/ofg/bye/bye.json'))
-    public static final MappingDescriptor ADMIN_STUB_DESCRIPTOR = new MappingDescriptor(new File(MAPPINGS_REPOSITORY_LOCATION, 'com/ofg/bye/admin/admin.json'))
-    public static final MappingDescriptor PL_BYE_STUB_DESCRIPTOR = new MappingDescriptor(new File(MAPPINGS_REPOSITORY_LOCATION, 'pl/com/ofg/bye/pl_bye.json'), REALM_SPECIFIC)
-    public static final MappingDescriptor PL_OVERRIDDEN_BYE_STUB_DESCRIPTOR = new MappingDescriptor(new File(MAPPINGS_REPOSITORY_LOCATION, 'pl/com/ofg/bye/pl_overridden_bye.json'), REALM_SPECIFIC)
 
     def 'should retrieve all descriptors for given project (both from main and realm specific contexts)'() {
         given:
-            StubRepository repository = new StubRepository(REPOSITORY_LOCATION)
+            StubRepository repository = new StubRepository(new File(repositoryLocation))
             ProjectMetadata projectMetadata = new ProjectMetadata('bye', 'com/ofg/bye', 'pl')
-            List<MappingDescriptor> expectedDescriptors = [MAIN_BYE_STUB_DESCRIPTOR, ADMIN_STUB_DESCRIPTOR , PL_BYE_STUB_DESCRIPTOR, PL_OVERRIDDEN_BYE_STUB_DESCRIPTOR]
+
+            File mappingsRepositoryLocation = new File(repositoryLocation, descriptorMappingsSubPath)
+            MappingDescriptor mainByeStubDescriptor = new MappingDescriptor(new File(mappingsRepositoryLocation, 'com/ofg/bye/bye.json'))
+            MappingDescriptor adminStubDescriptor = new MappingDescriptor(new File(mappingsRepositoryLocation, 'com/ofg/bye/admin/admin.json'))
+            MappingDescriptor plByeStubDescriptor = new MappingDescriptor(new File(mappingsRepositoryLocation, 'pl/com/ofg/bye/pl_bye.json'), REALM_SPECIFIC)
+            MappingDescriptor plOverriddenByeStubDescriptor = new MappingDescriptor(new File(mappingsRepositoryLocation, 'pl/com/ofg/bye/pl_overridden_bye.json'), REALM_SPECIFIC)
+
+            List<MappingDescriptor> expectedDescriptors = [mainByeStubDescriptor, adminStubDescriptor, plByeStubDescriptor, plOverriddenByeStubDescriptor]
+
         when:
             List<MappingDescriptor> descriptors = repository.getProjectDescriptors(projectMetadata)
+
         then:
             descriptors.size() == expectedDescriptors.size()
             descriptors.containsAll(expectedDescriptors)
+
+        where:
+            repositoryLocation                            | descriptorMappingsSubPath
+            'src/test/resources/repository'               | 'mappings'
+            'src/test/resources/deepenMappingsRepository' | 'deeper/located/mappings'
     }
 
     def 'should return empty list if files are missing'() {
@@ -33,4 +41,5 @@ class StubRepositorySpec extends Specification {
         then:
             descriptors.empty
     }
+
 }

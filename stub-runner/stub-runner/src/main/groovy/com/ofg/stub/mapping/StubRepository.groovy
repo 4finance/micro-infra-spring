4d74bb83cb0d1056a2f712556a59d05513b78cea
Adrian Woźniak <wozniak.adrian@gmail.com>
2017-05-19 10:46:37 +0200

    Weakening subs repository layout restrictions (#615)

--- stub-runner/stub-runner/src/main/groovy/com/ofg/stub/mapping/StubRepository.groovy
+++ stub-runner/stub-runner/src/main/groovy/com/ofg/stub/mapping/StubRepository.groovy
@@ -1,16 +1,22 @@
 package com.ofg.stub.mapping
 
 import groovy.transform.CompileStatic
-import groovy.transform.PackageScope
 
 @CompileStatic
 class StubRepository {
-    @PackageScope static final String DEFAULT_DESCRIPTORS_DIRECTORY = 'mappings'
+    static final String DEFAULT_DESCRIPTORS_DIRECTORY = 'mappings'
 
     private final DescriptorRepository descriptorRepository
 
     StubRepository(File repositoryRoot) {
         File descriptorDirectory = new File(repositoryRoot, DEFAULT_DESCRIPTORS_DIRECTORY)
+        if (!descriptorDirectory.exists()) {
+            repositoryRoot.eachDirRecurse {
+                if (it.name == DEFAULT_DESCRIPTORS_DIRECTORY) {
+                    descriptorDirectory = it
+                }
+            }
+        }
         descriptorRepository = new DescriptorRepository(descriptorDirectory)
     }
 

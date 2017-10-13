79af83f27dd852f7cb6b00901eb94414b3634611
matus.hennel <matus.hennel@4finance.com>
2017-10-13 14:28:14 +0200

    replace deprecations

--- activity-tracker-root/activity-tracker-boot-starter/src/test/java/io/fourfinance/activity_tracker/autoconfigure/ActivityTrackerAutoConfigurationDisabledTest.java
+++ activity-tracker-root/activity-tracker-boot-starter/src/test/java/io/fourfinance/activity_tracker/autoconfigure/ActivityTrackerAutoConfigurationDisabledTest.java
@@ -1,17 +1,16 @@
 package io.fourfinance.activity_tracker.autoconfigure;
 
-import static org.assertj.core.api.BDDAssertions.then;
-
+import io.fourfinance.activity_tracker.testapp.TestApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.ConfigFileApplicationContextInitializer;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import io.fourfinance.activity_tracker.testapp.TestApplication;
+import static org.assertj.core.api.BDDAssertions.then;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(value = TestApplication.class, initializers = ConfigFileApplicationContextInitializer.class)

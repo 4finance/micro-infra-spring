1014a4a98e937102d14171f83dab1cd1dbcc8d66
Adrian Woźniak <wozniak.adrian@gmail.com>
2017-12-27 09:11:56 +0100

    Fix for CONFIG_FOLDER property tests when set in environment

--- micro-infra-spring-config/src/test/groovy/com/ofg/infrastructure/property/LogbackConfigurationSpec.groovy
+++ micro-infra-spring-config/src/test/groovy/com/ofg/infrastructure/property/LogbackConfigurationSpec.groovy
@@ -1,13 +1,26 @@
 package com.ofg.infrastructure.property
 
+import org.junit.Rule
+import org.junit.contrib.java.lang.system.EnvironmentVariables
 import spock.lang.Specification
+import spock.lang.Subject
 import spock.util.environment.RestoreSystemProperties
 
 import static com.ofg.infrastructure.property.AppCoordinates.CONFIG_FOLDER
 
+@RestoreSystemProperties
 class LogbackConfigurationSpec extends Specification {
 
+    @Rule
+    EnvironmentVariables environmentVariables = new EnvironmentVariables()
+
+    @Subject
     LogbackConfiguration config = new LogbackConfiguration()
+
+    def setup() {
+        environmentVariables.set(CONFIG_FOLDER, null)
+        System.clearProperty(CONFIG_FOLDER)
+    }
 
     def 'log pattern should contain correlationId'() {
         expect:
@@ -34,7 +47,6 @@ class LogbackConfigurationSpec extends Specification {
             config.getRollingMaxHistory() > 0
     }
 
-    @RestoreSystemProperties
     def 'should read logger configuration from global.properties file'() {
         given:
             System.setProperty(CONFIG_FOLDER, getConfigFolder())

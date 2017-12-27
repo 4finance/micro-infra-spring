aa28eacbeada2cf76873389d21931563486bcbba
Piotr Pietrzak <piotr@pietrzak.me>
2017-12-27 10:06:59 +0100

    Revert "Fix for CONFIG_FOLDER property tests when set in environment"

--- micro-infra-spring-config/src/test/groovy/com/ofg/infrastructure/property/PropertiesFolderFinderSpec.groovy
+++ micro-infra-spring-config/src/test/groovy/com/ofg/infrastructure/property/PropertiesFolderFinderSpec.groovy
@@ -1,33 +1,21 @@
 package com.ofg.infrastructure.property
 
-import org.junit.Rule
-import org.junit.contrib.java.lang.system.EnvironmentVariables
 import spock.lang.Specification
-import spock.lang.Subject
 import spock.util.environment.RestoreSystemProperties
 
 import static com.ofg.infrastructure.property.AppCoordinates.CONFIG_FOLDER
 import static com.ofg.infrastructure.property.PropertiesFolderFinder.DEFAULT_CONFIG_DIR
 
-@RestoreSystemProperties
 class PropertiesFolderFinderSpec extends Specification {
 
-    @Rule
-    EnvironmentVariables environmentVariables = new EnvironmentVariables()
-
-    @Subject
     PropertiesFolderFinder propertiesFolderFinder = new PropertiesFolderFinder()
-
-    def setup() {
-        environmentVariables.set(CONFIG_FOLDER, null)
-        System.clearProperty(CONFIG_FOLDER)
-    }
 
     def 'should return default config folder when CONFIG_FOLDER is not set'() {
         expect:
             propertiesFolderFinder.find() == DEFAULT_CONFIG_DIR
     }
 
+    @RestoreSystemProperties
     def 'should return config folder from CONFIG_FOLDER variable'() {
         given:
             System.setProperty(CONFIG_FOLDER, 'properties')
